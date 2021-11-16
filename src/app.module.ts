d1d745bc418c3abea7aa9bf386065d7953c2115f
pezholio <pezholio@gmail.com>
2021-11-16 11:42:22 +0000

    Add TypeOrm config for database
    This adds TypeORM and related packages, as
    well as the necessary basic config to get it up
    and running with Postgres. I've also added a setup
    script that creates/drops the test and dev
    databases, creates env files, installs the
    dependencies, runs the migrations and builds the
    assets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
+process.env.NODE_ENV ||= 'development';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import dbConfiguration from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfiguration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...(await configService.get('database')),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/db.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from '@nestjs/config';
+import { config as setConfig } from 'dotenv';
+
+setConfig({ path: `.env.${process.env.NODE_ENV}` });
+
+export default registerAs('database', () => {
+  return {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: ['./dist/**/*.entity.js'],
+    migrations: ['./dist/db/migrate/*.js'],
+    migrationsRun: true,
+    dropSchema: process.env.NODE_ENV == 'test',
+    cli: {
+      migrationsDir: 'src/db/migrate',
+    },
+  };
+});
