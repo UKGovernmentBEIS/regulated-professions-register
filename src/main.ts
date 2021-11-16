0a9c09dcdae59af3182d4e4697e5d975ef014285
pezholio <pezholio@gmail.com>
2021-11-16 11:53:52 +0000

    Add Nunjucks as the templating language

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as nunjucks from 'nunjucks';
+import * as path from 'path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const express = app.getHttpAdapter().getInstance();
+
+  const assets = path.join(__dirname, '..', 'public');
+  const views = path.join(__dirname, '..', 'views');
+
+  nunjucks.configure(views, { express });
+
+  app.useStaticAssets(assets);
+  app.setBaseViewsDir(views);
+  app.setViewEngine('njk');
+
   await app.listen(3000);
 }
 bootstrap();
