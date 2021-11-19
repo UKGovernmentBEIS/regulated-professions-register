cf79c0a2c0a706a383381fe494c32474896ec484
pezholio <pezholio@gmail.com>
2021-11-19 14:15:22 +0000

    Add a migration to create a users table

---
+++ src/db/migrate/1637317177058-create_users.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUsers1637317177058 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // required for `uuid_generate_v4()`
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isUnique: true,
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'identifier',
+            type: 'varchar',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE users`);
+  }
+}
