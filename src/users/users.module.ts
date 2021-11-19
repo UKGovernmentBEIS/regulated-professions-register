fb8ac7464f1e34b2eab6dda4206411256554508a
pezholio <pezholio@gmail.com>
2021-11-19 11:50:01 +0000

    Add a user service
    This interacts with the Users entity and lets us fetch
     objects from the database. Eventually, this will
    include methods to add and edit objects in the
    database too.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
+import { UserService } from './user.service';
+
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
 })
 export class UsersModule {}
