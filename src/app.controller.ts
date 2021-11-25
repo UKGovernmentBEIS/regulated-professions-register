0f1639572561c87c0dbdfc18b40abb0085550fa3
pezholio <pezholio@gmail.com>
2021-11-25 12:19:19 +0000

    Build a simple admin endpoint
    This is an /admin controller endpoint that is guarded
    by our `AuthenticationGuard`. It renders a template
    that uses an admin layout and shows the user's
    name.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, UseGuards, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { AuthenticationGuard } from './common/authentication.guard';
+import { Request } from 'express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,5 +10,14 @@ export class AppController {
   @Render('show')
   getHello(): object {
     return {};
+  }
+
+  @Get('/admin')
+  @UseGuards(AuthenticationGuard)
+  @Render('admin/dashboard')
+  admin(@Req() req: Request): object {
+    return {
+      name: req.oidc.user.nickname,
+    };
   }
 }
