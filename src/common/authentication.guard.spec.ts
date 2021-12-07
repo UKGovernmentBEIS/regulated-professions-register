4026207e59ac31a3ea1b675186565cb0f2416ad1
pezholio <pezholio@gmail.com>
2021-12-07 17:04:20 +0000

    Update Authentication guard to check roles
    This updates the Authentication guard to check for
    a role (set in the metadata) and, if one exists, check
    this against the logged in user's roles.

--- src/common/authentication.guard.spec.ts
+++ src/common/authentication.guard.spec.ts
@@ -1,12 +1,17 @@
 import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
 import { createMock } from '@golevelup/ts-jest';
 import { AuthenticationGuard } from './authentication.guard';
+import { UserRole, User } from '../users/user.entity';
 
 describe('AuthenticationGuard', () => {
   let host: ExecutionContext;
   let oidc: any;
   let guard: AuthenticationGuard;
+  let reflector: Reflector;
+  let roles: string[];
+  let user: User;
 
   beforeEach(() => {
     host = createMock<ExecutionContext>({
@@ -14,12 +19,20 @@ describe('AuthenticationGuard', () => {
         getRequest: () => {
           return {
             oidc: oidc,
+            appSession: {
+              user: user,
+            },
           };
         },
       }),
     });
+    reflector = createMock<Reflector>({
+      get: () => {
+        return roles;
+      },
+    });
 
-    guard = new AuthenticationGuard();
+    guard = new AuthenticationGuard(reflector);
   });
 
   describe('when isAuthenticated() is true', () => {
@@ -31,6 +44,33 @@ describe('AuthenticationGuard', () => {
 
     it('should return true', () => {
       expect(guard.canActivate(host)).toStrictEqual(true);
+    });
+
+    describe('when roles are specified', () => {
+      beforeEach(() => {
+        roles = [UserRole.Admin];
+      });
+
+      it('should return true when the user has the appropriate role', () => {
+        user = new User();
+        user.roles = [UserRole.Admin];
+
+        expect(guard.canActivate(host)).toStrictEqual(true);
+      });
+
+      it('should return false when the user does not have the appropriate role', () => {
+        user = new User();
+        user.roles = [UserRole.Editor];
+
+        expect(guard.canActivate(host)).toStrictEqual(false);
+      });
+
+      it('should return false when the user has no roles', () => {
+        user = new User();
+        user.roles = [];
+
+        expect(guard.canActivate(host)).toStrictEqual(false);
+      });
     });
   });
 
--- src/common/authentication.guard.ts
+++ src/common/authentication.guard.ts
@@ -4,6 +4,8 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { User, UserRole } from '../users/user.entity';
 
 /**
  * Checks if a user is authenticated and returns `true` or `false`
@@ -15,7 +17,12 @@ import {
  * It uses the `canActivate` methods to get the request from the execution
  * context and first checks if the `isAuthenticated` method exists and then if
  * it is true or false.
- * *
+ *
+ * If the user is logged in, it also checks any metadata (added by the
+ * `Roles` metadata in a controller) to match these roles against the
+ * roles held by a logged in user. If the user does not have the expected
+ * role, we return false, which in turn, raises a `ForbiddenException`
+ *
  * @example (in a controller, as metadata above a controller action)
  *
  * @UseGuards(AuthenticationGuard)
@@ -25,7 +32,10 @@ import {
  */
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<UserRole[]>('roles', context.getHandler());
     const request = context.switchToHttp().getRequest();
     const loggedIn: boolean =
       typeof request?.oidc?.isAuthenticated !== 'undefined' &&
@@ -35,6 +45,16 @@ export class AuthenticationGuard implements CanActivate {
       throw new UnauthorizedException();
     }
 
-    return loggedIn;
+    if (!roles) {
+      return loggedIn;
+    } else {
+      const user = request.appSession.user as User;
+
+      return this.matchRoles(user, roles);
+    }
+  }
+
+  matchRoles(user: User, roles: UserRole[]): boolean {
+    return user.roles.some((role: UserRole) => roles.includes(role));
   }
 }
