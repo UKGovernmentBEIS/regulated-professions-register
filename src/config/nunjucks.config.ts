f8c2edeb87109eb7d11f81e3c1066c04ddfeda6b
pezholio <pezholio@gmail.com>
2021-12-08 16:20:58 +0000

    Add a helper to use translations in views
    This adds a Nunjucks filter to use our
    I18nService to use translations in views. They
    work like so:
    
    ```
    '$TRANSLATION' | t
    ```
    
    Where $TRANSLATION is a reference to the
    i18n translation.

--- src/config/nunjucks.config.ts
+++ src/config/nunjucks.config.ts
@@ -5,6 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as nunjucks from 'nunjucks';
 
 import { AssetsHelper } from '../helpers/assets.helper';
+import { I18nHelper } from '../helpers/i18n.helper';
 
 export const nunjucksConfig = async (
   app: NestExpressApplication,
@@ -13,6 +14,7 @@ export const nunjucksConfig = async (
   const express = app.getHttpAdapter().getInstance();
   const entrypoints = require('../../public/entrypoints.json');
   const assetsHelper = new AssetsHelper(entrypoints);
+  const i18nHelper = new I18nHelper(app);
 
   const env = nunjucks.configure(views, {
     noCache: process.env.NODE_ENV === 'development' ? true : false,
@@ -23,6 +25,18 @@ export const nunjucksConfig = async (
   env.addGlobal(
     'encore_entry_script_tags',
     await assetsHelper.entryScriptTags(),
+  );
+  env.addFilter(
+    't',
+    async (text, callback) => {
+      try {
+        const result = await i18nHelper.translate(text);
+        callback(null, result);
+      } catch (error) {
+        callback(error);
+      }
+    },
+    true,
   );
 
   return env;
