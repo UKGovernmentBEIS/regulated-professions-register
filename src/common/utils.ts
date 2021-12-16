4096600f4192b2e56ddedcea2874c2f464e6e650
bob walker <bob@dxw.com>
2021-12-16 11:33:11 +0000

    Remove test code from the utils module
    `createMockRequest` relies on a dev dependency which fails production builds.
    To fix this we have split it out to its own module and told `nest build` to
    ignore it in `tsconfig.build.json`.

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,4 @@
 import { Request } from 'express';
-import { createMock, DeepMocked } from '@golevelup/ts-jest';
 
 export function backLink(req: Request): string {
   const referrer = req.get('Referrer') || '';
@@ -8,19 +7,4 @@ export function backLink(req: Request): string {
   if (referrer.match('^https?://' + host)) {
     return referrer;
   }
-}
-
-export function createMockRequest(
-  referrer: string,
-  host: string,
-): DeepMocked<Request> {
-  return createMock<Request>({
-    get: (header) => {
-      if (header === 'Referrer') {
-        return referrer;
-      } else if (header === 'host') {
-        return host;
-      }
-    },
-  });
 }
