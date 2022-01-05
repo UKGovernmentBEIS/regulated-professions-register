68615a00c7a20fa8003ccf18eb436d56c7651ac3
pezholio <pezholio@gmail.com>
2022-01-05 09:34:10 +0000

    Use the `ENVIRONMENT` env var in seeds
    If our NODE_ENV is prod, we then use the
    ENVIRONMENT env var to either import the
    staging or prod data, according to what env
    we're currently on.

--- src/common/decorators/seeds.decorator.ts
+++ src/common/decorators/seeds.decorator.ts
@@ -2,9 +2,14 @@ import { Seeder } from 'nestjs-seeder';
 
 export function InjectData(filename: string) {
   return function (target: Seeder, propertyKey: string) {
-    const environment = process.env['NODE_ENV'] || 'development';
-    const path = `../../../seeds/${environment}/${filename}.json`;
+    const nodeEnv = process.env['NODE_ENV'] || 'development';
+    const environment = process.env['ENVIRONMENT'];
     let value: Array<any>;
+
+    const path =
+      nodeEnv === 'production'
+        ? `../../../seeds/${environment}/${filename}.json`
+        : `../../../seeds/${nodeEnv}/${filename}.json`;
 
     try {
       /* eslint-disable @typescript-eslint/no-var-requires */
