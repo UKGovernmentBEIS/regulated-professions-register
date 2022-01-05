1941d0f294b9b2c5f5094ed48d7567d9324526d4
pezholio <pezholio@gmail.com>
2022-01-05 14:15:55 +0000

    Send failing Notify API calls to Rollbar
    As this happens outside of Express, these
    won't get automatically sent. We can also send
    additional data, such as the email address and
    the template ID.

--- src/common/mailer.consumer.spec.ts
+++ src/common/mailer.consumer.spec.ts
@@ -1,9 +1,13 @@
 import { MailerConsumer } from './mailer.consumer';
 import { NotifyClient } from 'notifications-node-client';
+import Rollbar from 'rollbar';
+
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 
 describe('MailerConsumer', () => {
   let notifyClient: DeepMocked<NotifyClient>;
+  let rollbarClient: DeepMocked<Rollbar>;
+
   let mailerConsumer: MailerConsumer;
 
   beforeEach(() => {
@@ -12,11 +16,24 @@ describe('MailerConsumer', () => {
         return {};
       }),
     });
+    rollbarClient = createMock<Rollbar>({
+      error: jest.fn(() => {
+        return {};
+      }),
+    });
     mailerConsumer = new MailerConsumer();
 
     const getClient = jest.spyOn(MailerConsumer.prototype as any, 'getClient');
+    const getRollbarClient = jest.spyOn(
+      MailerConsumer.prototype as any,
+      'getRollbarClient',
+    );
+
     getClient.mockImplementation(() => {
       return notifyClient;
+    });
+    getRollbarClient.mockImplementation(() => {
+      return rollbarClient;
     });
   });
 
@@ -42,6 +59,31 @@ describe('MailerConsumer', () => {
         },
         reference: null,
       },
+    );
+  });
+
+  it('sends an error to rollbar if it fails', async () => {
+    notifyClient.sendEmail.mockImplementation(() => {
+      throw new Error();
+    });
+
+    await expect(async () => {
+      await mailerConsumer.send({
+        data: {
+          templateID: '1234',
+          email: 'email@example.com',
+          personalisation: {
+            subject: 'foo',
+            body: 'bar',
+          },
+        },
+      });
+    }).rejects.toThrowError();
+
+    expect(rollbarClient.error).toHaveBeenCalledWith(
+      expect.any(Error),
+      {},
+      { templateID: '1234', email: 'email@example.com' },
     );
   });
 });
