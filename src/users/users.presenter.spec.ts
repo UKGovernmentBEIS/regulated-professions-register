6c54d69beae43c57744532289def294793928af6
George Eaton <george@dxw.com>
2022-01-06 15:42:00 +0000

    Use factory to test users presenter

--- src/users/users.presenter.spec.ts
+++ src/users/users.presenter.spec.ts
@@ -1,35 +1,20 @@
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { I18nService } from 'nestjs-i18n';
+import userFactory from '../testutils/factories/user';
 
-import { User } from './user.entity';
 import { UsersPresenter } from './users.presenter';
 
-const users = [
-  createMock<User>({
-    id: 'some-uuid-string',
-    name: 'name',
-    email: 'email@example.com',
-    externalIdentifier: '212121',
-  }),
-  createMock<User>({
-    id: 'some-other-uuid-string',
-    name: 'name2',
-    email: 'emai2l@example.com',
-    externalIdentifier: '12345',
-  }),
-];
-
 describe('UsersPresenter', () => {
-  let presenter: UsersPresenter;
   let i18nService: DeepMocked<I18nService>;
 
   beforeEach(() => {
     i18nService = createMock<I18nService>();
-    presenter = new UsersPresenter(users, i18nService);
   });
 
   describe('tableRows', () => {
     it('returns an array of table rows', () => {
+      const users = userFactory.buildList(2);
+      const presenter = new UsersPresenter(users, i18nService);
       const rows = presenter.tableRows();
 
       expect(rows.length).toEqual(2);
