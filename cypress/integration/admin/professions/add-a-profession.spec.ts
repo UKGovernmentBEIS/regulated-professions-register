98db669dd9cee60846dcbf03401eefdbfc779331
George Eaton <george@dxw.com>
2022-01-12 16:52:09 +0000

    Display Qualification on Check your answers

--- cypress/integration/admin/professions/add-a-profession.spec.ts
+++ cypress/integration/admin/professions/add-a-profession.spec.ts
@@ -121,6 +121,18 @@ describe('Adding a new profession', () => {
       cy.get('body').should('contain', 'An example activity');
       cy.get('body').should('contain', 'A description of the regulation');
 
+      cy.get('body').should('contain', 'An example Qualification level');
+      cy.get('body').should('contain', 'Another method');
+      cy.translate(
+        'professions.form.radioButtons.methodsToObtainQualification.generalSecondaryEducation',
+      ).then((method) => {
+        cy.get('body').should('contain', method);
+      });
+      cy.get('body').should('contain', '4.0 Years');
+      cy.translate('app.yes').then((yes) => {
+        cy.get('body').should('contain', yes);
+      });
+
       cy.translate('professions.form.button.create').then((buttonText) => {
         cy.get('button').contains(buttonText).click();
       });
