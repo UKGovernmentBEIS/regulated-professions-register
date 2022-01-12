1b094f72022e644c231a823ad294d2680993dc0b
pezholio <pezholio@gmail.com>
2022-01-12 16:30:40 +0000

    Add presenters for a table of organisations
    This adds a presenter class to show a table
    of organisations. Actions have been left
    deliberately blank for now.

---
+++ src/organisations/organisation.presenter.ts
@@ -0,0 +1,58 @@
+import { I18nService } from 'nestjs-i18n';
+import { Organisation } from './organisation.entity';
+import { TableRow } from '../common/interfaces/table-row';
+
+export class OrganisationPresenter {
+  constructor(
+    private readonly organisation: Organisation,
+    private readonly i18nService: I18nService,
+  ) {}
+
+  public async tableRow(): Promise<TableRow> {
+    return [
+      {
+        text: this.organisation.name,
+      },
+      {
+        text: this.organisation.alternateName,
+      },
+      {
+        html: await this.industries(),
+      },
+      {
+        text: '',
+      },
+    ];
+  }
+
+  private async industries(): Promise<string> {
+    const professions = this.organisation.professions;
+
+    if (professions === undefined) {
+      throw new Error(
+        "You must eagerly load professions to show industries. Try adding `{ relations: ['professions'] }` to your finder in the `OrganisationsService` class",
+      );
+    }
+
+    const industries = professions
+      .map((profession) => {
+        if (profession.industries === undefined) {
+          throw new Error(
+            "You must eagerly load industries to show industries. Try adding `{ relations: ['professions.industries'] }` to your finder in the `OrganisationsService` class",
+          );
+        }
+
+        return profession.industries;
+      })
+      .flat();
+
+    const industryNames = await Promise.all(
+      industries.map(
+        (industry) =>
+          this.i18nService.translate(industry.name) as Promise<string>,
+      ),
+    );
+
+    return [...new Set(industryNames)].join('<br />');
+  }
+}
---
+++ src/organisations/organisations.presenter.ts
@@ -0,0 +1,44 @@
+import { I18nService } from 'nestjs-i18n';
+import { Organisation } from './organisation.entity';
+import { OrganisationPresenter } from './organisation.presenter';
+import { TableRow } from '../common/interfaces/table-row';
+import { Table } from '../common/interfaces/table';
+
+type Field = 'name' | 'alternateName' | 'industries' | 'actions';
+const fields = ['name', 'alternateName', 'industries', 'actions'] as Field[];
+
+export class OrganisationsPresenter {
+  constructor(
+    private readonly organisations: Organisation[],
+    private readonly i18nService: I18nService,
+  ) {}
+
+  async table(firstCellIsHeader = true): Promise<Table> {
+    return {
+      firstCellIsHeader: firstCellIsHeader,
+      head: await this.headers(),
+      rows: await Promise.all(
+        this.organisations.map(
+          async (organisation) =>
+            await new OrganisationPresenter(
+              organisation,
+              this.i18nService,
+            ).tableRow(),
+        ),
+      ),
+    };
+  }
+
+  private async headers(): Promise<TableRow> {
+    return (
+      await Promise.all(
+        fields.map(
+          (field) =>
+            this.i18nService.translate(
+              `organisations.admin.tableHeading.${field}`,
+            ) as Promise<string>,
+        ),
+      )
+    ).map((text) => ({ text }));
+  }
+}
