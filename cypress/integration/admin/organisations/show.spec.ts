1917835a2f42a7251e0a8f447a804ce64b61be7c
George Eaton <george@dxw.com>
2022-01-24 15:13:00 +0000

    Add a11y check to internal org listing tests

--- cypress/integration/admin/organisations/show.spec.ts
+++ cypress/integration/admin/organisations/show.spec.ts
@@ -2,11 +2,12 @@ describe('Showing organisations', () => {
   context('When I am logged in as admin', () => {
     beforeEach(() => {
       cy.loginAuth0('admin');
-      cy.visit('/admin');
+      cy.visitAndCheckAccessibility('/admin');
     });
 
     it('Shows the detail of an organisation, with its professions and a link to the admin profession page', () => {
       cy.get('a').contains('Regulatory authorities').click();
+      cy.checkAccessibility();
 
       cy.readFile('./seeds/test/professions.json').then((professions) => {
         cy.readFile('./seeds/test/organisations.json').then((organisations) => {
@@ -18,6 +19,7 @@ describe('Showing organisations', () => {
               cy.get('a').contains('View details').click();
             })
             .then(() => {
+              cy.checkAccessibility();
               cy.get('body').should('contain', organisation.name);
               cy.get('body').should('contain', organisation.email);
               cy.get('body').should('contain', organisation.contactUrl);
@@ -33,6 +35,7 @@ describe('Showing organisations', () => {
               });
 
               cy.contains(professionsForOrganisation[0].name).click();
+              cy.checkAccessibility();
               cy.get('body').should('contain', 'Edit this profession');
             });
         });
