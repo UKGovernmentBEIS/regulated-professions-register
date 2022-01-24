ff08c72cf5fce0292a30f058d61cd95326b9d5b7
Bryan Gale <bryan.gale@dxw.com>
2022-01-24 15:21:00 +0000

    Implement organisations filtering for logged in users
    This uses the `_horizontal-filter` and `createFilterInput` we implemented earlier

--- src/organisations/admin/presenters/organisations.presenter.spec.ts
+++ src/organisations/admin/presenters/organisations.presenter.spec.ts
@@ -1,18 +1,14 @@
-import { DeepMocked } from '@golevelup/ts-jest';
-import { I18nService } from 'nestjs-i18n';
-
 import organisationFactory from '../../../testutils/factories/organisation';
 import { OrganisationsPresenter } from './organisations.presenter';
 import { createMockI18nService } from '../../../testutils/create-mock-i18n-service';
 import { translationOf } from '../../../testutils/translation-of';
+import industryFactory from '../../../testutils/factories/industry';
+import { FilterInput } from '../../../common/interfaces/filter-input.interface';
+import { IndustriesCheckboxPresenter } from '../../../industries/industries-checkbox.presenter';
+import { OrganisationPresenter } from '../../presenters/organisation.presenter';
 
-const mockTableRow = () => {
-  return [
-    {
-      text: 'Some text',
-    },
-  ];
-};
+const mockTableRow = jest.fn();
+const mockCheckboxArgs = jest.fn();
 
 jest.mock('../../presenters/organisation.presenter', () => {
   return {
@@ -24,33 +20,147 @@ jest.mock('../../presenters/organisation.presenter', () => {
   };
 });
 
+jest.mock('../../../industries/industries-checkbox.presenter', () => {
+  return {
+    IndustriesCheckboxPresenter: jest.fn().mockImplementation(() => {
+      return {
+        checkboxArgs: mockCheckboxArgs,
+      };
+    }),
+  };
+});
+
 describe('OrganisationsPresenter', () => {
-  const i18nService: DeepMocked<I18nService> = createMockI18nService();
+  describe('present', () => {
+    describe('when called with empty `FilterInput`', () => {
+      it('returns `IndexTemplate` template data', async () => {
+        mockTableRow.mockReturnValue([
+          {
+            text: 'Some text',
+          },
+        ]);
 
-  describe('table', () => {
-    it('returns data for a table', async () => {
-      const organisations = organisationFactory.buildList(5);
-      const presenter = new OrganisationsPresenter(organisations, i18nService);
-      const table = await presenter.table();
+        mockCheckboxArgs.mockReturnValue([
+          {
+            text: 'Some text',
+            value: 'Some value',
+            checked: true,
+          },
+        ]);
 
-      expect(table.firstCellIsHeader).toEqual(true);
-      expect(table.head).toEqual([
-        { text: translationOf('organisations.admin.tableHeading.name') },
-        {
-          text: translationOf('organisations.admin.tableHeading.alternateName'),
-        },
-        {
-          text: translationOf('organisations.admin.tableHeading.industries'),
-        },
-        { text: translationOf('organisations.admin.tableHeading.actions') },
-      ]);
-      expect(table.rows).toEqual([
-        mockTableRow(),
-        mockTableRow(),
-        mockTableRow(),
-        mockTableRow(),
-        mockTableRow(),
-      ]);
+        const i18nService = createMockI18nService();
+
+        const industries = industryFactory.buildList(3);
+        const organisations = organisationFactory.buildList(5);
+
+        const filterInput: FilterInput = {};
+
+        const presenter = new OrganisationsPresenter(
+          industries,
+          filterInput,
+          organisations,
+          i18nService,
+        );
+        const result = await presenter.present();
+
+        expect(OrganisationPresenter).toHaveBeenCalledTimes(5);
+        expect(OrganisationPresenter).toHaveBeenLastCalledWith(
+          organisations[4],
+          i18nService,
+        );
+
+        expect(result.organisationsTable.firstCellIsHeader).toEqual(true);
+        expect(result.organisationsTable.head).toEqual([
+          { text: translationOf('organisations.admin.tableHeading.name') },
+          {
+            text: translationOf(
+              'organisations.admin.tableHeading.alternateName',
+            ),
+          },
+          {
+            text: translationOf('organisations.admin.tableHeading.industries'),
+          },
+          { text: translationOf('organisations.admin.tableHeading.actions') },
+        ]);
+        expect(result.organisationsTable.rows).toEqual([
+          mockTableRow(),
+          mockTableRow(),
+          mockTableRow(),
+          mockTableRow(),
+          mockTableRow(),
+        ]);
+
+        expect(result.industriesCheckboxArgs).toEqual(mockCheckboxArgs());
+      });
+
+      it('returns empty filter data', async () => {
+        mockTableRow.mockReturnValue([]);
+        mockCheckboxArgs.mockReturnValue([]);
+
+        const i18nService = createMockI18nService();
+
+        const industries = industryFactory.buildList(3);
+        const organisations = organisationFactory.buildList(5);
+
+        const filterInput: FilterInput = {};
+
+        const presenter = new OrganisationsPresenter(
+          industries,
+          filterInput,
+          organisations,
+          i18nService,
+        );
+
+        const result = await presenter.present();
+
+        expect(IndustriesCheckboxPresenter).toBeCalledWith(
+          industries,
+          [],
+          i18nService,
+        );
+
+        expect(result.filters).toEqual({
+          keywords: '',
+          industries: [],
+        });
+      });
+    });
+
+    describe('when called with populated `FilterInput`', () => {
+      it('returns populated filter data', async () => {
+        mockTableRow.mockReturnValue([]);
+        mockCheckboxArgs.mockReturnValue([]);
+
+        const i18nService = createMockI18nService();
+
+        const industries = industryFactory.buildList(3);
+        const organisations = organisationFactory.buildList(5);
+
+        const filterInput: FilterInput = {
+          keywords: 'example keywords',
+          industries: [industries[0], industries[2]],
+        };
+
+        const presenter = new OrganisationsPresenter(
+          industries,
+          filterInput,
+          organisations,
+          i18nService,
+        );
+
+        const result = await presenter.present();
+
+        expect(IndustriesCheckboxPresenter).toBeCalledWith(
+          industries,
+          [industries[0], industries[2]],
+          i18nService,
+        );
+
+        expect(result.filters).toEqual({
+          keywords: 'example keywords',
+          industries: [industries[0].name, industries[2].name],
+        });
+      });
     });
   });
 });
