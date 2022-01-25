f2ec85fd0f25286a40e6d6d6be9df73b0beb4588
George Eaton <george@dxw.com>
2022-01-25 14:22:21 +0000

    Refactor Professions controller tests
    This makes them more inline with the patterns we've been following more
    recently:
    
    - Not creating top-level objects, but setting them up in tests, with the
      exception of `i18nService` and `professionsService` which we don't
      really need to care about (as we stub return values in the tests
      themselves).
    - Using `translationOf` rather than specific values we set up on the
      `mockI18nService`.

--- src/professions/professions.controller.spec.ts
+++ src/professions/professions.controller.spec.ts
@@ -3,6 +3,7 @@ import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { I18nService } from 'nestjs-i18n';
 import QualificationPresenter from '../qualifications/presenters/qualification.presenter';
+import { createMockI18nService } from '../testutils/create-mock-i18n-service';
 import industryFactory from '../testutils/factories/industry';
 import professionFactory from '../testutils/factories/profession';
 import { translationOf } from '../testutils/translation-of';
@@ -10,26 +11,14 @@ import { translationOf } from '../testutils/translation-of';
 import { ProfessionsController } from './professions.controller';
 import { ProfessionsService } from './professions.service';
 
-const industry = industryFactory.build({ name: 'industries.example' });
-const exampleProfession = professionFactory.build({
-  id: 'profession-id',
-  name: 'Example Profession',
-  occupationLocations: ['GB-ENG'],
-  industries: [industry],
-});
-
 describe('ProfessionsController', () => {
   let controller: ProfessionsController;
   let professionsService: DeepMocked<ProfessionsService>;
   let i18nService: DeepMocked<I18nService>;
 
   beforeEach(async () => {
-    professionsService = createMock<ProfessionsService>({
-      save: async () => {
-        return exampleProfession;
-      },
-    });
-    i18nService = createMock<I18nService>();
+    professionsService = createMock<ProfessionsService>();
+    i18nService = createMockI18nService();
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -45,45 +34,32 @@ describe('ProfessionsController', () => {
     controller = module.get<ProfessionsController>(ProfessionsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
   describe('show', () => {
     it('should return populated template params', async () => {
-      professionsService.findBySlug.mockImplementationOnce(
-        async () => exampleProfession,
-      );
-
-      i18nService.translate.mockImplementation(async (text) => {
-        switch (text) {
-          case 'industries.example':
-            return 'Example industry';
-          case 'nations.england':
-            return 'England';
-          default:
-            return '';
-        }
+      const industry = industryFactory.build({ name: 'industries.example' });
+      const profession = professionFactory.build({
+        id: 'profession-id',
+        name: 'Example Profession',
+        occupationLocations: ['GB-ENG'],
+        industries: [industry],
       });
+
+      professionsService.findBySlug.mockResolvedValue(profession);
 
       const result = await controller.show('example-slug');
 
       expect(result).toEqual({
-        profession: exampleProfession,
-        qualification: new QualificationPresenter(
-          exampleProfession.qualification,
-        ),
-        nations: ['England'],
-        industries: ['Example industry'],
+        profession: profession,
+        qualification: new QualificationPresenter(profession.qualification),
+        nations: [translationOf('nations.england')],
+        industries: [translationOf('industries.example')],
       });
 
       expect(professionsService.findBySlug).toBeCalledWith('example-slug');
     });
 
     it('should throw an error when the slug does not match a profession', () => {
-      professionsService.findBySlug.mockImplementationOnce(async () => {
-        return null;
-      });
+      professionsService.findBySlug.mockResolvedValue(null);
 
       expect(async () => {
         await controller.show('example-invalid-slug');
