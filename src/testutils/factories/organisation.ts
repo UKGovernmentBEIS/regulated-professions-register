28c7ef2e5c23cadafd3cef6fed6bad082609bcc9
pezholio <pezholio@gmail.com>
2022-01-26 10:23:19 +0000

    Fix factories
    This fixes the factories to work with the
    entities as they are now set up

--- src/testutils/factories/organisation.ts
+++ src/testutils/factories/organisation.ts
@@ -4,16 +4,9 @@ import { Organisation } from '../../organisations/organisation.entity';
 export default Factory.define<Organisation>(({ sequence }) => ({
   id: sequence.toString(),
   name: 'Example Organisation',
-  alternateName: 'Alternate Organisation Name',
   slug: 'example-slug',
-  address: '123 Fake Street, London, AB1 2AB, England',
-  url: 'https://www.example-org.com',
-  email: 'hello@example-org.com',
-  contactUrl: 'https://www.example-org.com/contact-us',
-  telephone: '+441234567890',
-  fax: '+441234567891',
   professions: undefined,
-  version: undefined,
+  versions: [],
   created_at: new Date(),
   updated_at: new Date(),
 }));
