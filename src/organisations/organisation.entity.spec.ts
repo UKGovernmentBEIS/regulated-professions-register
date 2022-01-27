348bc64ceb8e5c302ae7e2bad4879c94fa270f0a
pezholio <pezholio@gmail.com>
2022-01-27 17:07:06 +0000

    Add a withVersion method to Organisation
    This allows us to combine an organisation
    and its version together easily in one
    place

---
+++ src/organisations/organisation.entity.spec.ts
@@ -0,0 +1,32 @@
+import { Organisation } from './organisation.entity';
+
+import organisationFactory from '../testutils/factories/organisation';
+import organisationVersionFactory from '../testutils/factories/organisation-version';
+
+describe('Organisation', () => {
+  describe('withVersion', () => {
+    it('should return an entity with a version', () => {
+      const organisationVersion = organisationVersionFactory.build();
+      const organisation = organisationFactory.build({
+        versions: [organisationVersion, organisationVersionFactory.build()],
+      });
+
+      const result = Organisation.withVersion(
+        organisation,
+        organisationVersion,
+      );
+
+      expect(result).toEqual({
+        ...organisation,
+        alternateName: organisationVersion.alternateName,
+        address: organisationVersion.address,
+        url: organisationVersion.url,
+        email: organisationVersion.email,
+        contactUrl: organisationVersion.contactUrl,
+        telephone: organisationVersion.telephone,
+        fax: organisationVersion.fax,
+        versionId: organisationVersion.id,
+      });
+    });
+  });
+});
