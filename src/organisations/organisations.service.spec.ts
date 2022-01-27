bff77d0605239661a06bb2862e152a18276a2b5e
pezholio <pezholio@gmail.com>
2022-01-27 17:06:31 +0000

    Add a findWithVersion method
    This searches for an organisation and a
    version by their respective IDs and returns
    a complete Organisation object with a
    versionId.

--- src/organisations/organisations.service.spec.ts
+++ src/organisations/organisations.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException } from '@nestjs/common';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { createMock } from '@golevelup/ts-jest';
 
 import { Repository } from 'typeorm';
 import organisationFactory from '../testutils/factories/organisation';
+import organisationVersionFactory from '../testutils/factories/organisation-version';
 import professionFactory from '../testutils/factories/profession';
 import { Organisation } from './organisation.entity';
 import { OrganisationsService } from './organisations.service';
+import { OrganisationVersion } from './organisation-version.entity';
 
 describe('OrganisationsService', () => {
   let service: OrganisationsService;
@@ -104,6 +107,64 @@ describe('OrganisationsService', () => {
 
       expect(foundOrganisation).toEqual(organisation);
       expect(repoSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('findWithVersion', () => {
+    it('returns an Organisation with version data', async () => {
+      const versionAttributes = {
+        alternateName: 'Alternate Organisation Name',
+        address: '123 Fake Street, London, AB1 2AB, England',
+        url: 'https://www.example-org.com',
+        email: 'hello@example-org.com',
+        contactUrl: 'https://www.example-org.com/contact-us',
+        telephone: '+441234567890',
+        fax: '+441234567891',
+      };
+      const version = organisationVersionFactory.build(versionAttributes);
+
+      const organisation = organisationFactory.build({
+        versions: [version, organisationVersionFactory.build()],
+      });
+
+      const repoSpy = jest
+        .spyOn(repo, 'findOne')
+        .mockResolvedValue(organisation);
+
+      const result = await service.findWithVersion(organisation.id, version.id);
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          ...versionAttributes,
+          id: organisation.id,
+          versionId: version.id,
+        }),
+      );
+
+      expect(repoSpy).toHaveBeenCalledWith({
+        relations: ['versions'],
+        where: { id: organisation.id },
+      });
+    });
+
+    it('raises a NotFoundException if the organisation cannot be found', async () => {
+      jest.spyOn(repo, 'findOne').mockResolvedValue(undefined);
+
+      await expect(async () => {
+        await service.findWithVersion(organisation.id, '1234');
+      }).rejects.toThrow(NotFoundException);
+    });
+
+    it('raises a NotFoundException if the version cannot be found', async () => {
+      const organisation = organisationFactory.build({
+        versions: [organisationVersionFactory.build()],
+      });
+
+      jest.spyOn(repo, 'findOne').mockResolvedValue(organisation);
+
+      await expect(async () => {
+        await service.findWithVersion(organisation.id, '1234');
+      }).rejects.toThrow(NotFoundException);
     });
   });
 
