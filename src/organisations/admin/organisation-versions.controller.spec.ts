652ea170d8e42e1452f1182d6d971c57e954615c
pezholio <pezholio@gmail.com>
2022-01-28 12:19:24 +0000

    Allow a new version to be created on edit
    This fetches the latest draft or live version,
    makes a copy, and redirects to the edit
    action for that organisation and version.
    
    Now all this is in place, we can restore the
    Organisation edit e2e specs and link to th
    new versions endpoint when editing.

--- src/organisations/admin/organisation-versions.controller.spec.ts
+++ src/organisations/admin/organisation-versions.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Response } from 'express';
+import { DeepPartial } from 'fishery';
 
 import { OrganisationVersionsController } from './organisation-versions.controller';
 
@@ -7,6 +9,10 @@ import { OrganisationVersionsService } from '../organisation-versions.service';
 import { OrganisationsService } from '../organisations.service';
 
 import organisationFactory from '../../testutils/factories/organisation';
+import organisationVersionFactory from '../../testutils/factories/organisation-version';
+import userFactory from '../../testutils/factories/user';
+
+import { RequestWithAppSession } from '../../common/interfaces/request-with-app-session.interface';
 
 describe('OrganisationVersionsController', () => {
   let controller: OrganisationVersionsController;
@@ -51,6 +57,41 @@ describe('OrganisationVersionsController', () => {
       expect(result).toEqual(organisation);
 
       expect(organisationsService.find).toHaveBeenCalledWith(organisation.id);
+    });
+  });
+
+  describe('create', () => {
+    it('creates a copy of the latest version of the organisation', async () => {
+      const organisationVersion = organisationVersionFactory.build();
+      const user = userFactory.build();
+
+      const response = createMock<Response>();
+      const request = createMock<RequestWithAppSession>({
+        appSession: {
+          user: user as DeepPartial<any>,
+        },
+      });
+
+      organisationVersionsService.findLatestForOrganisationId.mockResolvedValue(
+        organisationVersion,
+      );
+      organisationVersionsService.save.mockResolvedValue(organisationVersion);
+
+      await controller.create(response, request, 'some-uuid');
+
+      expect(organisationVersionsService.save).toHaveBeenCalledWith(
+        expect.objectContaining({
+          id: undefined,
+          user: user,
+          created_at: undefined,
+          updated_at: undefined,
+          organisation: organisationVersion.organisation,
+        }),
+      );
+
+      expect(response.redirect).toHaveBeenCalledWith(
+        `/admin/organisations/${organisationVersion.organisation.id}/versions/${organisationVersion.id}/edit`,
+      );
     });
   });
 });
