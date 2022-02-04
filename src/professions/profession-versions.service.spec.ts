bf19d338d525088cb3096cfcfdec5fdae5005da6
George Eaton <george@dxw.com>
2022-02-04 11:31:51 +0000

    Add an allDraftOrLive service method
     This fetches all the versions, and adds a DISTINCT ON query to make
     sure we only get one version per Profession. This is then ordered by
     the created_at date to make sure the version we get is the latest one.
     We then map the versions to create a Profession class instance with all
     the expected data.

--- src/professions/profession-versions.service.spec.ts
+++ src/professions/profession-versions.service.spec.ts
@@ -160,6 +160,55 @@ describe('ProfessionVersionsService', () => {
     });
   });
 
+  describe('allDraftOrLive', () => {
+    it('gets all Professions and their latest draft or live version', async () => {
+      const versions = professionVersionFactory.buildList(5);
+      const queryBuilder = createMock<SelectQueryBuilder<ProfessionVersion>>({
+        leftJoinAndSelect: () => queryBuilder,
+        where: () => queryBuilder,
+        distinctOn: () => queryBuilder,
+        orderBy: () => queryBuilder,
+        getMany: async () => versions,
+      });
+
+      jest
+        .spyOn(repo, 'createQueryBuilder')
+        .mockImplementation(() => queryBuilder);
+
+      const result = await service.allDraftOrLive();
+
+      const expectedProfessions = versions.map((version) =>
+        Profession.withVersion(version.profession, version),
+      );
+
+      expect(result).toEqual(expectedProfessions);
+
+      expect(queryBuilder.distinctOn).toHaveBeenCalledWith([
+        'professionVersion.profession',
+      ]);
+
+      expect(queryBuilder).toHaveJoined([
+        'professionVersion.profession',
+        'professionVersion.organisation',
+        'professionVersion.industries',
+        'professionVersion.qualification',
+        'professionVersion.legislations',
+      ]);
+
+      expect(queryBuilder.where).toHaveBeenCalledWith(
+        'professionVersion.status IN(:...status)',
+        {
+          status: [ProfessionVersionStatus.Live, ProfessionVersionStatus.Draft],
+        },
+      );
+
+      expect(queryBuilder.orderBy).toHaveBeenCalledWith(
+        'professionVersion.profession, professionVersion.created_at',
+        'DESC',
+      );
+    });
+  });
+
   describe('findLiveBySlug', () => {
     it('fetches a live Profession by its slug', async () => {
       const version = professionVersionFactory.build();
