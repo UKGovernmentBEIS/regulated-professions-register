b66cbcf6a7442f208fbc454c3ad4313b1b84ecaf
pezholio <pezholio@gmail.com>
2022-02-09 11:14:21 +0000

    Add an edit e2e test
    I've also changed the name of the existing
    tests to be more in line with what we have
    for Organisations.

---
+++ cypress/integration/admin/user/edit.ts
@@ -0,0 +1,56 @@
+describe('Editing a user', () => {
+  context('when I am not logged in', () => {
+    it('I am prompted to log in', () => {
+      cy.visitAndCheckAccessibility('/admin/users');
+      cy.location('pathname').should('contain', 'login');
+    });
+  });
+
+  context('when I am logged in', () => {
+    beforeEach(() => {
+      cy.loginAuth0();
+    });
+
+    it('I can edit a user', () => {
+      cy.visitAndCheckAccessibility('/admin/users');
+      cy.contains('View Editor').click();
+
+      cy.checkAccessibility();
+
+      cy.contains('Change Organisation').click();
+      cy.checkAccessibility();
+
+      cy.translate('users.form.headings.edit').then((heading) => {
+        cy.get('body').should('contain', heading);
+      });
+
+      cy.get('input[name="serviceOwner"][value="0"]').check();
+
+      cy.get('select[name="organisation"]').select('Department for Education');
+
+      cy.get('button').click();
+      cy.checkAccessibility();
+
+      cy.translate('users.form.label.organisation').then(
+        (organisationLabel) => {
+          cy.get('dt')
+            .contains(organisationLabel)
+            .then(($key) => {
+              const $row = $key.parent();
+
+              cy.wrap($row).should('contain', 'Department for Education');
+            });
+        },
+      );
+
+      cy.get('button').click();
+      cy.checkAccessibility();
+
+      cy.translate('users.headings.edit.done').then((successMessage) => {
+        cy.get('body')
+          .should('contain', successMessage)
+          .should('contain', 'beis-rpr+editor@dxw.com');
+      });
+    });
+  });
+});
