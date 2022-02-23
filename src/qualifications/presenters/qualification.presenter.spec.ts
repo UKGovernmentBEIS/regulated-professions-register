129ab1c01bb139e5619726fa196b6dd11afc955e
Bryan Gale <bryan.gale@dxw.com>
2022-02-23 12:12:17 +0000

    Fix `QualificationPresenter` failing when a Profession has only a name and organisation

--- src/qualifications/presenters/qualification.presenter.spec.ts
+++ src/qualifications/presenters/qualification.presenter.spec.ts
@@ -8,51 +8,13 @@ import { translationOf } from '../../testutils/translation-of';
 jest.mock('../../helpers/format-multiline-string.helper');
 
 describe(QualificationPresenter, () => {
-  describe('routesToObtain', () => {
-    it('returns the a multiline string of the text value', () => {
-      (formatMultilineString as jest.Mock).mockImplementation(multilineOf);
+  describe('when the Qualification is defined', () => {
+    describe('routesToObtain', () => {
+      it('returns the a multiline string of the text value', () => {
+        (formatMultilineString as jest.Mock).mockImplementation(multilineOf);
 
-      const qualification = qualificationFactory.build({
-        routesToObtain: 'other value',
-      });
-
-      const presenter = new QualificationPresenter(
-        qualification,
-        createMockI18nService(),
-      );
-
-      expect(presenter.routesToObtain).toEqual(multilineOf('other value'));
-
-      expect(formatMultilineString).toBeCalledWith('other value');
-    });
-  });
-
-  describe('mostCommonRouteToObtain', () => {
-    it('returns the a multiline string of the text value', () => {
-      (formatMultilineString as jest.Mock).mockImplementation(multilineOf);
-
-      const qualification = qualificationFactory.build({
-        mostCommonRouteToObtain: 'other value',
-      });
-
-      const presenter = new QualificationPresenter(
-        qualification,
-        createMockI18nService(),
-      );
-
-      expect(presenter.mostCommonRouteToObtain).toEqual(
-        multilineOf('other value'),
-      );
-
-      expect(formatMultilineString).toBeCalledWith('other value');
-    });
-  });
-
-  describe('mandatoryProfessionalExperience', () => {
-    describe('when true', () => {
-      it('returns the localisation id for "Yes"', () => {
         const qualification = qualificationFactory.build({
-          mandatoryProfessionalExperience: true,
+          routesToObtain: 'other value',
         });
 
         const presenter = new QualificationPresenter(
@@ -60,14 +22,18 @@ describe(QualificationPresenter, () => {
           createMockI18nService(),
         );
 
-        expect(presenter.mandatoryProfessionalExperience).toEqual('app.yes');
+        expect(presenter.routesToObtain).toEqual(multilineOf('other value'));
+
+        expect(formatMultilineString).toBeCalledWith('other value');
       });
     });
 
-    describe('when false', () => {
-      it('returns the localisation id for "No"', () => {
+    describe('mostCommonRouteToObtain', () => {
+      it('returns the a multiline string of the text value', () => {
+        (formatMultilineString as jest.Mock).mockImplementation(multilineOf);
+
         const qualification = qualificationFactory.build({
-          mandatoryProfessionalExperience: false,
+          mostCommonRouteToObtain: 'other value',
         });
 
         const presenter = new QualificationPresenter(
@@ -75,109 +41,162 @@ describe(QualificationPresenter, () => {
           createMockI18nService(),
         );
 
-        expect(presenter.mandatoryProfessionalExperience).toEqual('app.no');
+        expect(presenter.mostCommonRouteToObtain).toEqual(
+          multilineOf('other value'),
+        );
+
+        expect(formatMultilineString).toBeCalledWith('other value');
       });
     });
 
-    describe('when not set at all on a blank Qualification', () => {
-      it('returns an empty string', () => {
-        const qualification = qualificationFactory.build({
-          mandatoryProfessionalExperience: null,
+    describe('mandatoryProfessionalExperience', () => {
+      describe('when true', () => {
+        it('returns the localisation id for "Yes"', () => {
+          const qualification = qualificationFactory.build({
+            mandatoryProfessionalExperience: true,
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.mandatoryProfessionalExperience).toEqual('app.yes');
         });
-
-        const presenter = new QualificationPresenter(
-          qualification,
-          createMockI18nService(),
-        );
-
-        expect(presenter.mandatoryProfessionalExperience).toEqual('');
       });
-    });
-  });
 
-  describe('moreInformationUrl', () => {
-    describe('when a blank string is provided', () => {
-      it('returns null', () => {
-        const qualification = qualificationFactory.build({
-          url: '',
+      describe('when false', () => {
+        it('returns the localisation id for "No"', () => {
+          const qualification = qualificationFactory.build({
+            mandatoryProfessionalExperience: false,
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.mandatoryProfessionalExperience).toEqual('app.no');
         });
-
-        const presenter = new QualificationPresenter(
-          qualification,
-          createMockI18nService(),
-        );
-
-        expect(presenter.moreInformationUrl).toEqual(null);
       });
-    });
-    describe('when a URL is provided', () => {
-      it('returns a link', () => {
-        const qualification = qualificationFactory.build({
-          url: 'http://example.com',
+
+      describe('when not set at all on a blank Qualification', () => {
+        it('returns an empty string', () => {
+          const qualification = qualificationFactory.build({
+            mandatoryProfessionalExperience: null,
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.mandatoryProfessionalExperience).toEqual('');
         });
-
-        const presenter = new QualificationPresenter(
-          qualification,
-          createMockI18nService(),
-        );
-
-        expect(presenter.moreInformationUrl).toEqual(
-          '<a class="govuk-link" href="http://example.com">http://example.com</a>',
-        );
       });
     });
-  });
 
-  describe('ukRecognitionUrl', () => {
-    describe('when a blank string is provided', () => {
-      it('returns null', () => {
-        const qualification = qualificationFactory.build({
-          ukRecognitionUrl: '',
+    describe('moreInformationUrl', () => {
+      describe('when a blank string is provided', () => {
+        it('returns null', () => {
+          const qualification = qualificationFactory.build({
+            url: '',
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.moreInformationUrl).toEqual(null);
         });
-
-        const presenter = new QualificationPresenter(
-          qualification,
-          createMockI18nService(),
-        );
-
-        expect(presenter.ukRecognitionUrl).toEqual(null);
       });
-    });
-    describe('when a URL is provided', () => {
-      it('returns a link', () => {
-        const qualification = qualificationFactory.build({
-          ukRecognitionUrl: 'http://example.com',
+      describe('when a URL is provided', () => {
+        it('returns a link', () => {
+          const qualification = qualificationFactory.build({
+            url: 'http://example.com',
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.moreInformationUrl).toEqual(
+            '<a class="govuk-link" href="http://example.com">http://example.com</a>',
+          );
         });
-
-        const presenter = new QualificationPresenter(
-          qualification,
-          createMockI18nService(),
-        );
-
-        expect(presenter.ukRecognitionUrl).toEqual(
-          '<a class="govuk-link" href="http://example.com">http://example.com</a>',
-        );
       });
     });
-  });
 
-  describe('otherCountriesRecognitionUrl', () => {
-    describe('when a blank string is provided', () => {
-      it('returns null', () => {
-        const qualification = qualificationFactory.build({
-          otherCountriesRecognitionUrl: '',
+    describe('ukRecognitionUrl', () => {
+      describe('when a blank string is provided', () => {
+        it('returns null', () => {
+          const qualification = qualificationFactory.build({
+            ukRecognitionUrl: '',
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.ukRecognitionUrl).toEqual(null);
         });
+      });
+      describe('when a URL is provided', () => {
+        it('returns a link', () => {
+          const qualification = qualificationFactory.build({
+            ukRecognitionUrl: 'http://example.com',
+          });
 
-        const presenter = new QualificationPresenter(
-          qualification,
-          createMockI18nService(),
-        );
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
 
-        expect(presenter.otherCountriesRecognitionUrl).toEqual(null);
+          expect(presenter.ukRecognitionUrl).toEqual(
+            '<a class="govuk-link" href="http://example.com">http://example.com</a>',
+          );
+        });
       });
     });
-    describe('when a URL is provided', () => {
-      it('returns a link', () => {
+
+    describe('otherCountriesRecognitionUrl', () => {
+      describe('when a blank string is provided', () => {
+        it('returns null', () => {
+          const qualification = qualificationFactory.build({
+            otherCountriesRecognitionUrl: '',
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.otherCountriesRecognitionUrl).toEqual(null);
+        });
+      });
+      describe('when a URL is provided', () => {
+        it('returns a link', () => {
+          const qualification = qualificationFactory.build({
+            otherCountriesRecognitionUrl: 'http://example.com',
+          });
+
+          const presenter = new QualificationPresenter(
+            qualification,
+            createMockI18nService(),
+          );
+
+          expect(presenter.otherCountriesRecognitionUrl).toEqual(
+            '<a class="govuk-link" href="http://example.com">http://example.com</a>',
+          );
+        });
+      });
+    });
+
+    describe('summaryList', () => {
+      it('returns a summary list of all Qualification fields', async () => {
         const qualification = qualificationFactory.build({
           otherCountriesRecognitionUrl: 'http://example.com',
         });
@@ -187,85 +206,91 @@ describe(QualificationPresenter, () => {
           createMockI18nService(),
         );
 
-        expect(presenter.otherCountriesRecognitionUrl).toEqual(
-          '<a class="govuk-link" href="http://example.com">http://example.com</a>',
-        );
+        expect(presenter.summaryList()).resolves.toEqual({
+          classes: 'govuk-summary-list--no-border',
+          rows: [
+            {
+              key: {
+                text: translationOf('professions.show.qualification.level'),
+              },
+              value: {
+                html: formatMultilineString(presenter.level),
+              },
+            },
+            {
+              key: {
+                text: translationOf(
+                  'professions.show.qualification.routesToObtain',
+                ),
+              },
+              value: {
+                html: presenter.routesToObtain,
+              },
+            },
+            {
+              key: {
+                text: translationOf(
+                  'professions.show.qualification.mostCommonRouteToObtain',
+                ),
+              },
+              value: {
+                html: presenter.mostCommonRouteToObtain,
+              },
+            },
+            {
+              key: {
+                text: translationOf('professions.show.qualification.duration'),
+              },
+              value: {
+                text: presenter.duration,
+              },
+            },
+            {
+              key: {
+                text: translationOf(
+                  'professions.show.qualification.mandatoryExperience',
+                ),
+              },
+              value: {
+                text: translationOf(presenter.mandatoryProfessionalExperience),
+              },
+            },
+            {
+              key: {
+                text: translationOf(
+                  'professions.show.qualification.moreInformationUrl',
+                ),
+              },
+              value: {
+                html: presenter.moreInformationUrl,
+              },
+            },
+          ],
+        });
       });
     });
   });
 
-  describe('summaryList', () => {
-    it('returns a summary list of all Qualification fields', async () => {
-      const qualification = qualificationFactory.build({
-        otherCountriesRecognitionUrl: 'http://example.com',
-      });
+  describe('when the Qualification is undefined', () => {
+    it('presents empty values', () => {
+      (formatMultilineString as jest.Mock).mockImplementation(multilineOf);
 
-      const presenter = new QualificationPresenter(
-        qualification,
-        createMockI18nService(),
+      expect(
+        new QualificationPresenter(undefined, createMockI18nService()),
+      ).toEqual(
+        expect.objectContaining({
+          duration: undefined,
+          level: undefined,
+          moreInformationUrl: null,
+          mostCommonRouteToObtain: multilineOf(undefined),
+          otherCountriesRecognition: undefined,
+          otherCountriesRecognitionUrl: null,
+          qualification: undefined,
+          routesToObtain: multilineOf(undefined),
+          ukRecognition: undefined,
+          ukRecognitionUrl: null,
+        }),
       );
-
-      expect(presenter.summaryList()).resolves.toEqual({
-        classes: 'govuk-summary-list--no-border',
-        rows: [
-          {
-            key: {
-              text: translationOf('professions.show.qualification.level'),
-            },
-            value: {
-              html: formatMultilineString(presenter.level),
-            },
-          },
-          {
-            key: {
-              text: translationOf(
-                'professions.show.qualification.routesToObtain',
-              ),
-            },
-            value: {
-              html: presenter.routesToObtain,
-            },
-          },
-          {
-            key: {
-              text: translationOf(
-                'professions.show.qualification.mostCommonRouteToObtain',
-              ),
-            },
-            value: {
-              html: presenter.mostCommonRouteToObtain,
-            },
-          },
-          {
-            key: {
-              text: translationOf('professions.show.qualification.duration'),
-            },
-            value: {
-              text: presenter.duration,
-            },
-          },
-          {
-            key: {
-              text: translationOf(
-                'professions.show.qualification.mandatoryExperience',
-              ),
-            },
-            value: {
-              text: translationOf(presenter.mandatoryProfessionalExperience),
-            },
-          },
-          {
-            key: {
-              text: translationOf(
-                'professions.show.qualification.moreInformationUrl',
-              ),
-            },
-            value: {
-              html: presenter.moreInformationUrl,
-            },
-          },
-        ],
-      });
     });
   });
 
