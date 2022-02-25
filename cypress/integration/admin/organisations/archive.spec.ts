aef0c0a835078e992a563f58a2c98017b4c37bfd
pezholio <pezholio@gmail.com>
2022-02-25 13:46:04 +0000

    Update the delete button to go to the confirm page
    Also, add an e2e test to test the whole
    archive journey.

---
+++ cypress/integration/admin/organisations/archive.spec.ts
@@ -0,0 +1,55 @@
+describe('Archiving organisations', () => {
+  context('When I am logged in as a registrar', () => {
+    beforeEach(() => {
+      cy.loginAuth0('registrar');
+      cy.visitAndCheckAccessibility('/admin');
+    });
+
+    it('Allows me to archive a draft organisation', () => {
+      cy.get('a').contains('Regulatory authorities').click();
+      cy.checkAccessibility();
+
+      cy.contains('Department for Education')
+        .parent('tr')
+        .within(() => {
+          cy.get('a').contains('View details').click();
+        });
+
+      cy.translate('organisations.status.draft').then((status) => {
+        cy.get('h2[data-status]').should('contain', status);
+      });
+
+      cy.translate('organisations.admin.button.archive').then(
+        (archiveButton) => {
+          cy.get('a').contains(archiveButton).click();
+        },
+      );
+
+      cy.checkAccessibility();
+
+      cy.translate('organisations.admin.archive.caption').then(
+        (archiveCaption) => {
+          cy.get('body').contains(archiveCaption);
+        },
+      );
+
+      cy.translate('organisations.admin.archive.heading', {
+        organisationName: 'Department for Education',
+      }).then((heading) => {
+        cy.contains(heading);
+      });
+
+      cy.translate('organisations.admin.button.archive').then((buttonText) => {
+        cy.get('button').contains(buttonText).click();
+      });
+
+      cy.checkAccessibility();
+
+      cy.translate('organisations.admin.archive.confirmation.heading').then(
+        (confirmation) => {
+          cy.get('html').should('contain', confirmation);
+        },
+      );
+    });
+  });
+});
