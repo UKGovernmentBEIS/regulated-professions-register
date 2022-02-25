ce88dc71f57cb6a792be0e1a4d7c5f00278d6fce
pezholio <pezholio@gmail.com>
2022-02-25 15:23:36 +0000

    Add a delete button to go to the confirm page
    Also, add an e2e test to test the whole
    professions archive journey.

---
+++ cypress/integration/admin/professions/archive.spec.ts
@@ -0,0 +1,53 @@
+import { format } from 'date-fns';
+
+describe('Archiving professions', () => {
+  context('When I am logged in as a registrar', () => {
+    beforeEach(() => {
+      cy.loginAuth0('registrar');
+      cy.visitAndCheckAccessibility('/admin');
+    });
+
+    it('Allows me to archive a draft profession', () => {
+      cy.get('a').contains('Regulated professions').click();
+      cy.checkAccessibility();
+
+      cy.contains('Gas Safe Engineer')
+        .parent('tr')
+        .within(() => {
+          cy.get('a').contains('View details').click();
+        });
+
+      cy.translate('professions.admin.status.draft').then((status) => {
+        cy.get('h2[data-status]').should('contain', status);
+      });
+
+      cy.translate('professions.admin.button.archive').then((button) => {
+        cy.get('a').contains(button).click();
+      });
+
+      cy.checkAccessibility();
+      cy.translate('professions.admin.archive.caption').then((caption) => {
+        cy.get('body').contains(caption);
+      });
+
+      cy.translate('professions.admin.archive.heading', {
+        professionName: 'Gas Safe Engineer',
+      }).then((heading) => {
+        cy.contains(heading);
+      });
+
+      cy.translate('professions.admin.button.archive').then((buttonText) => {
+        cy.get('button').contains(buttonText).click();
+      });
+
+      cy.checkAccessibility();
+
+      cy.translate('professions.admin.archive.confirmation.heading').then(
+        (confirmation) => {
+          cy.get('html').should('contain', confirmation);
+        },
+      );
+
+    });
+  });
+});
