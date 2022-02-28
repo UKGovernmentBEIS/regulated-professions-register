a56f919dd50b7e18ec8dc8d85460c455dc257885
Bryan Gale <bryan.gale@dxw.com>
2022-02-28 15:47:32 +0000

    Improve typing on `Validator`
    We want to use the preprocessed URL in our controllers, without repeating the preprocessing done during validation. So, we will use `obj` on `Validator`, but first we want to make sure that `obj` is correctly typed

--- src/helpers/validator.ts
+++ src/helpers/validator.ts
@@ -1,19 +1,22 @@
 import { validate, ValidationError } from 'class-validator';
 import { plainToClass } from 'class-transformer';
 
-export class Validator {
-  obj: object;
+export class Validator<T extends object> {
+  obj: T;
   dtoType: any;
   errors: ValidationError[];
 
-  public static async validate(dtoType: any, obj: object) {
-    const object: object = plainToClass(dtoType, obj);
+  public static async validate<T extends object>(
+    dtoType: { new (): T },
+    obj: object,
+  ) {
+    const object: T = plainToClass(dtoType, obj);
     const errors = await validate(object);
 
-    return new Validator(dtoType, obj, errors);
+    return new Validator<T>(dtoType, object, errors);
   }
 
-  constructor(dtoType: any, obj: object, errors: ValidationError[]) {
+  constructor(dtoType: any, obj: T, errors: ValidationError[]) {
     this.obj = obj;
     this.dtoType = dtoType;
     this.errors = errors;
