36ee4d62e068e9ba1f830d495848e85b4f73f314
George Eaton <george@dxw.com>
2022-03-07 17:31:51 +0000

    Ensure we're sanitising html in flash message

--- src/common/flash-message.spec.ts
+++ src/common/flash-message.spec.ts
@@ -1,27 +1,41 @@
+import { escape } from '../helpers/escape.helper';
+import { escapeOf } from '../testutils/escape-of';
 import { flashMessage } from './flash-message';
+
+jest.mock('../helpers/escape.helper');
 
 describe('flashMessage', () => {
   describe('when a body and title are provided', () => {
     it('returns the html with a body and title', () => {
+      (escape as jest.Mock).mockImplementation(escapeOf);
       const response = flashMessage('Title', 'Body');
 
       expect(response).toMatch(
-        '<h3 class="govuk-notification-banner__heading">Title</h3>',
+        `<h3 class="govuk-notification-banner__heading">${escapeOf(
+          'Title',
+        )}</h3>`,
       );
 
-      expect(response).toMatch('<p class="govuk-body">Body</p>');
+      expect(response).toMatch(`<p class="govuk-body">${escapeOf('Body')}</p>`);
     });
   });
 
   describe('when the title is provided', () => {
     it('only returns the title', () => {
+      (escape as jest.Mock).mockImplementation(escapeOf);
       const response = flashMessage('Title');
 
       expect(response).toMatch(
-        '<h3 class="govuk-notification-banner__heading">Title</h3>',
+        `<h3 class="govuk-notification-banner__heading">${escapeOf(
+          'Title',
+        )}</h3>`,
       );
 
       expect(response).not.toMatch('<p class="govuk-body">');
     });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 });
--- src/common/flash-message.ts
+++ src/common/flash-message.ts
@@ -1,8 +1,12 @@
+import { escape } from '../helpers/escape.helper';
+
 export function flashMessage(heading: string, body?: string): string {
-  let html = `<h3 class="govuk-notification-banner__heading">${heading}</h3>`;
+  let html = `<h3 class="govuk-notification-banner__heading">${escape(
+    heading,
+  )}</h3>`;
 
   if (body) {
-    html += `<p class="govuk-body">${body}</p>`;
+    html += `<p class="govuk-body">${escape(body)}</p>`;
   }
 
   return html;
