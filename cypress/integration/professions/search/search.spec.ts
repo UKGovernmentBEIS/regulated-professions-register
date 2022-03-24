beb82d25117198bfd3de0d14282762f8b46f5d9a
Bryan Gale <bryan.gale@dxw.com>
2022-03-24 17:00:46 +0000

    Re-order public-facing search E2E tests for consistency

--- cypress/integration/professions/search/search.spec.ts
+++ cypress/integration/professions/search/search.spec.ts
@@ -52,6 +52,31 @@ describe('Searching a profession', () => {
     );
   });
 
+  it('I can filter by keyword', () => {
+    cy.get('input[name="keywords"]').type('Attorney');
+
+    cy.get('button').click();
+    cy.checkAccessibility();
+
+    cy.get('input[name="keywords"]').should('have.value', 'Attorney');
+
+    cy.get('body').should('contain', 'Registered Trademark Attorney');
+    cy.get('body').should(
+      'not.contain',
+      'Secondary School Teacher in State maintained schools (England)',
+    );
+  });
+
+  it('The search uses a professions keywords to filter', () => {
+    cy.get('input[name="keywords"]').type('Adjudicator');
+    cy.get('button').click();
+    cy.get('body').should('contain', 'Registered Trademark Attorney');
+    cy.get('body').should(
+      'not.contain',
+      'Secondary School Teacher in State maintained schools (England)',
+    );
+  });
+
   it('I can filter by nation', () => {
     cy.get('input[name="nations[]"][value="GB-WLS"]').check();
 
@@ -115,31 +140,6 @@ describe('Searching a profession', () => {
       'Secondary School Teacher in State maintained schools (England)',
     );
     cy.get('body').should('not.contain', 'Registered Trademark Attorney');
-  });
-
-  it('I can filter by keyword', () => {
-    cy.get('input[name="keywords"]').type('Attorney');
-
-    cy.get('button').click();
-    cy.checkAccessibility();
-
-    cy.get('input[name="keywords"]').should('have.value', 'Attorney');
-
-    cy.get('body').should('contain', 'Registered Trademark Attorney');
-    cy.get('body').should(
-      'not.contain',
-      'Secondary School Teacher in State maintained schools (England)',
-    );
-  });
-
-  it('The search uses a professions keywords to filter', () => {
-    cy.get('input[name="keywords"]').type('Adjudicator');
-    cy.get('button').click();
-    cy.get('body').should('contain', 'Registered Trademark Attorney');
-    cy.get('body').should(
-      'not.contain',
-      'Secondary School Teacher in State maintained schools (England)',
-    );
   });
 
   it('I can use the back button to go back to my search results', () => {
