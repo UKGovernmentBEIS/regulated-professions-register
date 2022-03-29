e1081b459c8ea961b3ff024e87dfed56f62716a6
pezholio <pezholio@gmail.com>
2022-03-29 16:34:43 +0100

    Update `getOrganisationsFromProfession` to use new relation

--- src/professions/helpers/get-organisations-from-profession.helper.spec.ts
+++ src/professions/helpers/get-organisations-from-profession.helper.spec.ts
@@ -1,14 +1,17 @@
 import organisationFactory from '../../testutils/factories/organisation';
 import professionFactory from '../../testutils/factories/profession';
 import { getOrganisationsFromProfession } from './get-organisations-from-profession.helper';
+import { ProfessionToOrganisation } from '../profession-to-organisation.entity';
 
 describe('getOrganisationsFromProfession', () => {
-  describe('when the Organisation has only a main Organisation', () => {
+  describe('when the Organisation has one organisation', () => {
     it('returns a single-element array containing only that organisation', () => {
       const organisation = organisationFactory.build();
 
       const profession = professionFactory.build({
-        organisation,
+        professionToOrganisations: [
+          { organisation: organisation },
+        ] as ProfessionToOrganisation[],
       });
 
       expect(getOrganisationsFromProfession(profession)).toEqual([
@@ -17,14 +20,16 @@ describe('getOrganisationsFromProfession', () => {
     });
   });
 
-  describe('when the Organisation has a main Organisation and an additional Organisation', () => {
+  describe('when the Organisation has two organisations', () => {
     it('returns a two element array containing both organisations', () => {
       const organisation = organisationFactory.build();
       const additionalOrganisation = organisationFactory.build();
 
       const profession = professionFactory.build({
-        organisation,
-        additionalOrganisation,
+        professionToOrganisations: [
+          { organisation: organisation },
+          { organisation: additionalOrganisation },
+        ] as ProfessionToOrganisation[],
       });
 
       expect(getOrganisationsFromProfession(profession)).toEqual([
@@ -34,17 +39,19 @@ describe('getOrganisationsFromProfession', () => {
     });
   });
 
-  describe('when the Organisation has only an additional Organisation', () => {
-    it('returns a single-element array containing only that organisation', () => {
-      const additionalOrganisation = organisationFactory.build();
+  describe('when an organisation is null', () => {
+    it('filters any null items out', () => {
+      const organisation = organisationFactory.build();
 
       const profession = professionFactory.build({
-        organisation: undefined,
-        additionalOrganisation,
+        professionToOrganisations: [
+          { organisation: organisation },
+          { organisation: null },
+        ] as ProfessionToOrganisation[],
       });
 
       expect(getOrganisationsFromProfession(profession)).toEqual([
-        additionalOrganisation,
+        organisation,
       ]);
     });
   });
