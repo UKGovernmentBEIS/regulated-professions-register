c6c7f2c5565e9114e904e2673196cda58797ee81
pezholio <pezholio@gmail.com>
2022-03-29 16:41:12 +0100

    Use new relation on `checkCanViewProfession`

--- src/users/helpers/check-can-view-profession.spec.ts
+++ src/users/helpers/check-can-view-profession.spec.ts
@@ -3,14 +3,24 @@ import organisationFactory from '../../testutils/factories/organisation';
 import professionFactory from '../../testutils/factories/profession';
 import { checkCanViewOrganisation } from './check-can-view-organisation';
 import { checkCanViewProfession } from './check-can-view-profession';
+import { ProfessionToOrganisation } from './../../professions/profession-to-organisation.entity';
 
 jest.mock('./check-can-view-organisation');
 
 describe('checkCanViewProfession', () => {
   it('passes in a Profession with an organisation to call CheckCanViewOrganisation', () => {
-    const organisation = organisationFactory.build();
+    const organisation1 = organisationFactory.build();
+    const organisation2 = organisationFactory.build();
+
     const profession = professionFactory.build({
-      organisation: organisation,
+      professionToOrganisations: [
+        {
+          organisation: organisation1,
+        },
+        {
+          organisation: organisation2,
+        },
+      ] as ProfessionToOrganisation[],
     });
 
     const request = createDefaultMockRequest({ profession: profession });
@@ -18,7 +28,12 @@ describe('checkCanViewProfession', () => {
 
     expect(checkCanViewOrganisation).toHaveBeenCalledWith(
       request,
-      organisation,
+      organisation1,
+    );
+
+    expect(checkCanViewOrganisation).toHaveBeenCalledWith(
+      request,
+      organisation2,
     );
   });
 });
--- src/users/helpers/check-can-view-profession.ts
+++ src/users/helpers/check-can-view-profession.ts
@@ -1,10 +1,13 @@
 import { RequestWithAppSession } from '../../common/interfaces/request-with-app-session.interface';
 import { Profession } from '../../professions/profession.entity';
 import { checkCanViewOrganisation } from './check-can-view-organisation';
+import { getOrganisationsFromProfession } from '../../professions/helpers/get-organisations-from-profession.helper';
 
 export function checkCanViewProfession(
   request: RequestWithAppSession,
   profession: Profession,
 ) {
-  checkCanViewOrganisation(request, profession.organisation);
+  for (const org of getOrganisationsFromProfession(profession)) {
+    checkCanViewOrganisation(request, org);
+  }
 }
