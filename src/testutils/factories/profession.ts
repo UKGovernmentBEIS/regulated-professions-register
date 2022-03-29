3235fdedd4767005f7cf8f1b831f28e4029749c7
pezholio <pezholio@gmail.com>
2022-03-29 16:43:43 +0100

    Allow professions to be built with orgs easily
    This adds a new transient param to the professions
    factory, making it easier for us to build professions
    with organisations

--- src/testutils/factories/profession.ts
+++ src/testutils/factories/profession.ts
@@ -1,12 +1,20 @@
 import { Factory } from 'fishery';
 import { Profession } from '../../professions/profession.entity';
+import { Organisation } from '../../organisations/organisation.entity';
+
 import { RegulationType } from '../../professions/profession-version.entity';
+import { ProfessionToOrganisation } from '../../professions/profession-to-organisation.entity';
+
 import industryFactory from './industry';
 import organisationFactory from './organisation';
 import qualificationFactory from './qualification';
 import userFactory from './user';
 
-class ProfessionFactory extends Factory<Profession> {
+type ProfessionTransientParams = {
+  organisations: Organisation[];
+};
+
+class ProfessionFactory extends Factory<Profession, ProfessionTransientParams> {
   justCreated(id: string) {
     return this.params({
       id: id,
@@ -28,34 +36,53 @@ class ProfessionFactory extends Factory<Profession> {
   }
 }
 
-export default ProfessionFactory.define(({ sequence }) => ({
-  id: sequence.toString(),
-  name: 'Example Profession',
-  alternateName: 'Alternate profession name',
-  slug: 'example-slug',
-  description:
-    'A description of the profession that will be displayed to users',
-  occupationLocations: ['GB-ENG'],
-  regulationType: RegulationType.Licensing,
-  registrationRequirements: 'Some Requirements',
-  registrationUrl: 'http://example.com/required',
-  industries: [
-    industryFactory.build({ name: 'Example industry', id: 'example-industry' }),
-  ],
-  qualification: qualificationFactory.build(),
-  confirmed: false,
-  created_at: new Date(),
-  legislations: [],
-  organisation: organisationFactory.build({ name: 'Example organisation' }),
-  professionToOrganisations: undefined,
-  additionalOrganisation: undefined,
-  regulationSummary: 'Example summary',
-  reservedActivities: 'Example activities',
-  protectedTitles: 'Example titles',
-  regulationUrl: 'http://example.com/regulations',
-  versions: [],
-  changedByUser: userFactory.build(),
-  lastModified: new Date(),
-  updated_at: new Date(),
-  versionId: 'version-id',
-}));
+export default ProfessionFactory.define(({ transientParams, sequence }) => {
+  const { organisations } = transientParams;
+
+  const professionToOrganisations = (
+    organisations || [
+      organisationFactory.build({ name: 'Example organisation' }),
+    ]
+  ).map((organisation) => {
+    return {
+      organisation: organisation,
+    } as ProfessionToOrganisation;
+  });
+
+  const profession = {
+    id: sequence.toString(),
+    name: 'Example Profession',
+    alternateName: 'Alternate profession name',
+    slug: 'example-slug',
+    description:
+      'A description of the profession that will be displayed to users',
+    occupationLocations: ['GB-ENG'],
+    regulationType: RegulationType.Licensing,
+    registrationRequirements: 'Some Requirements',
+    registrationUrl: 'http://example.com/required',
+    industries: [
+      industryFactory.build({
+        name: 'Example industry',
+        id: 'example-industry',
+      }),
+    ],
+    qualification: qualificationFactory.build(),
+    confirmed: false,
+    created_at: new Date(),
+    legislations: [],
+    organisation: organisationFactory.build({ name: 'Example organisation' }),
+    professionToOrganisations: professionToOrganisations,
+    additionalOrganisation: undefined,
+    regulationSummary: 'Example summary',
+    reservedActivities: 'Example activities',
+    protectedTitles: 'Example titles',
+    regulationUrl: 'http://example.com/regulations',
+    versions: [],
+    changedByUser: userFactory.build(),
+    lastModified: new Date(),
+    updated_at: new Date(),
+    versionId: 'version-id',
+  };
+
+  return profession;
+});
