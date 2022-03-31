e0243552e64ceb0dfb263355ad73b4911b00b7ef
pezholio <pezholio@gmail.com>
2022-03-31 11:54:19 +0100

    Add role arguments to the select presenter
    This adds a new method to get a list of roles for
    a select input, as well as a method to return both
    organisations and roles.

--- src/professions/admin/presenters/regulated-authorities-select-presenter.ts
+++ src/professions/admin/presenters/regulated-authorities-select-presenter.ts
@@ -1,10 +1,13 @@
 import { SelectItemArgs } from '../../../common/interfaces/select-item-args.interface';
+import { AuthorityAndRoleArgs } from './../interfaces/authority-and-role-args';
 import { Organisation } from '../../../organisations/organisation.entity';
-
+import { OrganisationRole } from '../../profession-to-organisation.entity';
+import { I18nService } from 'nestjs-i18n';
 export class RegulatedAuthoritiesSelectPresenter {
   constructor(
     private readonly allOrganisations: Organisation[],
     private readonly selectedOrganisation: Organisation | null,
+    private readonly selectedRole?: OrganisationRole | null,
   ) {}
 
   selectArgs(): SelectItemArgs[] {
@@ -27,5 +30,34 @@ export class RegulatedAuthoritiesSelectPresenter {
     );
 
     return options;
+  }
+
+  async roleArgs(i18nService: I18nService): Promise<SelectItemArgs[]> {
+    const options = [
+      {
+        text: '--- Please Select ---',
+        value: '',
+        selected: null,
+      },
+    ];
+
+    for (const role of Object.values(OrganisationRole)) {
+      options.push({
+        text: await i18nService.translate(`organisations.label.roles.${role}`),
+        value: role,
+        selected: this.selectedRole ? this.selectedRole === role : false,
+      });
+    }
+
+    return options;
+  }
+
+  async authoritiesAndRoles(
+    i18nService: I18nService,
+  ): Promise<AuthorityAndRoleArgs> {
+    return {
+      authorities: this.selectArgs(),
+      roles: await this.roleArgs(i18nService),
+    };
   }
 }
