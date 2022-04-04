69f0de4fce4aa63b46f8dbc522f39b5f19b17efc
George Eaton <george@dxw.com>
2022-04-04 15:10:34 +0100

    Add `getNationsFromProfessions` helper
    This will be used to loop through the `occupationLocations` on a
    `Profession` and `stringify` them - it's based on the `nations` function
    on the `OrganisationPresenter` which we can replace with this function,
    so we can use it to display the nations in the Organisation search
    results.

--- src/helpers/nations.helper.spec.ts
+++ src/helpers/nations.helper.spec.ts
@@ -1,4 +1,8 @@
-import { allNations, isUK } from './nations.helper';
+import * as stringifyNationsModule from '../nations/helpers/stringifyNations';
+import { Nation } from '../nations/nation';
+import { createMockI18nService } from '../testutils/create-mock-i18n-service';
+import professionFactory from '../testutils/factories/profession';
+import { allNations, getNationsFromProfessions, isUK } from './nations.helper';
 
 describe('nations.helper', () => {
   describe('allNations', () => {
@@ -22,6 +26,74 @@ describe('nations.helper', () => {
 
         expect(isUK(nations)).toBeFalsy();
       });
+    });
+  });
+
+  describe('getNationsFromProfession', () => {
+    it('joins the nations on the given professions', async () => {
+      const i18nService = createMockI18nService();
+      const profession1NationCodes = ['GB-ENG', 'GB-WLS'];
+      const profession2NationCodes = ['GB-SCT'];
+
+      const england = new Nation('nations.england', 'GB-ENG');
+      const wales = new Nation('nations.wales', 'GB-WLS');
+      const scotland = new Nation('nations.scotland', 'GB-SCT');
+
+      jest.spyOn(Nation, 'all').mockReturnValue([england, scotland, wales]);
+
+      const professions = [
+        professionFactory.build({
+          occupationLocations: profession1NationCodes,
+        }),
+        professionFactory.build({
+          occupationLocations: profession2NationCodes,
+        }),
+      ];
+
+      const stringifyNationsSpy = jest.spyOn(
+        stringifyNationsModule,
+        'stringifyNations',
+      );
+
+      await getNationsFromProfessions(professions, i18nService);
+
+      expect(stringifyNationsSpy).toHaveBeenCalledWith(
+        [england, wales, scotland],
+        i18nService,
+      );
+    });
+
+    it('removes duplicate nations', async () => {
+      const i18nService = createMockI18nService();
+      const profession1NationCodes = ['GB-ENG', 'GB-WLS'];
+      const profession2NationCodes = ['GB-SCT', 'GB-ENG'];
+
+      const england = new Nation('nations.england', 'GB-ENG');
+      const wales = new Nation('nations.wales', 'GB-WLS');
+      const scotland = new Nation('nations.scotland', 'GB-SCT');
+
+      jest.spyOn(Nation, 'all').mockReturnValue([england, scotland, wales]);
+
+      const professions = [
+        professionFactory.build({
+          occupationLocations: profession1NationCodes,
+        }),
+        professionFactory.build({
+          occupationLocations: profession2NationCodes,
+        }),
+      ];
+
+      const stringifyNationsSpy = jest.spyOn(
+        stringifyNationsModule,
+        'stringifyNations',
+      );
+
+      await getNationsFromProfessions(professions, i18nService);
+
+      expect(stringifyNationsSpy).toHaveBeenCalledWith(
+        [england, wales, scotland],
+        i18nService,
+      );
     });
   });
 });
--- src/helpers/nations.helper.ts
+++ src/helpers/nations.helper.ts
@@ -1,3 +1,6 @@
+import { I18nService } from 'nestjs-i18n';
+import { stringifyNations } from '../nations/helpers/stringifyNations';
+import { Profession } from '../professions/profession.entity';
 import { Nation } from './../nations/nation';
 
 export function allNations(): string[] {
@@ -11,4 +14,17 @@ export function isUK(nationCodes: string[]) {
     nationCodes.length === nations.length &&
     nationCodes.every((e, i) => e === nations[i])
   );
+}
+
+export async function getNationsFromProfessions(
+  professions: Profession[],
+  i18nService: I18nService,
+): Promise<string> {
+  const nationCodes = professions
+    .map((profession) => profession.occupationLocations || [])
+    .flat();
+
+  const nations = [...new Set(nationCodes)].map((code) => Nation.find(code));
+
+  return await stringifyNations(nations, i18nService);
 }
