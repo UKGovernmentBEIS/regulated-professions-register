5ae7bf9f67902bce83a15b905d77bc3af7f5f2cf
Bryan Gale <bryan.gale@dxw.com>
2022-04-07 17:25:56 +0100

    Add Nunjucks template for showing decision data
    We will use this for displaying a single dataset within a year. We keep the bulk of this in a shared template, as will will want to display very similar content for both internal and external users

---
+++ src/decisions/admin/interfaces/show-template.interface.ts
@@ -0,0 +1,10 @@
+import { Table } from '../../../common/interfaces/table';
+import { Organisation } from '../../../organisations/organisation.entity';
+import { Profession } from '../../../professions/profession.entity';
+
+export interface ShowTemplate {
+  profession: Profession;
+  organisation: Organisation;
+  year: string;
+  tables: Table[];
+}
