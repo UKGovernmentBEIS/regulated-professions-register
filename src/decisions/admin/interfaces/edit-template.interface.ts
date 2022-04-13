0be6190aeed20d54409e05bc421523d59a4be621
Bryan Gale <bryan.gale@dxw.com>
2022-04-13 12:31:08 +0100

    Add a Nunjucks template for our data editing page
    We also add the associated template interface, DTO, and additions to our localisation JSON

---
+++ src/decisions/admin/interfaces/edit-template.interface.ts
@@ -0,0 +1,11 @@
+import { Organisation } from '../../../organisations/organisation.entity';
+import { Profession } from '../../../professions/profession.entity';
+import { RouteTemplate } from './route-template.interface';
+
+export interface EditTemplate {
+  profession: Profession;
+  organisation: Organisation;
+  year: string;
+
+  routes: RouteTemplate[];
+}
