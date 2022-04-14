22f3de34991bbf2782e77b2073c9c283b3fadd17
Bryan Gale <bryan.gale@dxw.com>
2022-04-14 11:13:46 +0100

    Fix formatting in our `CountrySelectPresenter` tests
    Fix a missing level of `describe`, and spelling of "returns"

--- src/decisions/admin/presenters/countries-select.presenter.spec.ts
+++ src/decisions/admin/presenters/countries-select.presenter.spec.ts
@@ -2,128 +2,130 @@ import { SelectItemArgs } from '../../../common/interfaces/select-item-args.inte
 import { CountriesSelectPresenter } from './countries-select.presenter';
 
 describe('CountrySelectPresenter', () => {
-  describe('when given a null selected country', () => {
-    it('retunrs a list of countries, with no country selected', () => {
-      const presenter = new CountriesSelectPresenter(null);
+  describe('selectArgs', () => {
+    describe('when given a null selected country', () => {
+      it('returns a list of countries, with no country selected', () => {
+        const presenter = new CountriesSelectPresenter(null);
 
-      const expected: SelectItemArgs[] = [
-        {
-          text: '--- Please Select ---',
-          value: '',
-          selected: null,
-        },
-        {
-          text: 'France',
-          value: 'France',
-          selected: false,
-        },
-        {
-          text: 'Belgium',
-          value: 'Belgium',
-          selected: false,
-        },
-        {
-          text: 'Brazil',
-          value: 'Brazil',
-          selected: false,
-        },
-        {
-          text: 'Japan',
-          value: 'Japan',
-          selected: false,
-        },
-        {
-          text: 'Morocco',
-          value: 'Morocco',
-          selected: false,
-        },
-        {
-          text: 'Poland',
-          value: 'Poland',
-          selected: false,
-        },
-        {
-          text: 'Germany',
-          value: 'Germany',
-          selected: false,
-        },
-        {
-          text: 'Italy',
-          value: 'Italy',
-          selected: false,
-        },
-        {
-          text: 'Canada',
-          value: 'Canada',
-          selected: false,
-        },
-      ];
+        const expected: SelectItemArgs[] = [
+          {
+            text: '--- Please Select ---',
+            value: '',
+            selected: null,
+          },
+          {
+            text: 'France',
+            value: 'France',
+            selected: false,
+          },
+          {
+            text: 'Belgium',
+            value: 'Belgium',
+            selected: false,
+          },
+          {
+            text: 'Brazil',
+            value: 'Brazil',
+            selected: false,
+          },
+          {
+            text: 'Japan',
+            value: 'Japan',
+            selected: false,
+          },
+          {
+            text: 'Morocco',
+            value: 'Morocco',
+            selected: false,
+          },
+          {
+            text: 'Poland',
+            value: 'Poland',
+            selected: false,
+          },
+          {
+            text: 'Germany',
+            value: 'Germany',
+            selected: false,
+          },
+          {
+            text: 'Italy',
+            value: 'Italy',
+            selected: false,
+          },
+          {
+            text: 'Canada',
+            value: 'Canada',
+            selected: false,
+          },
+        ];
 
-      const result = presenter.selectArgs();
+        const result = presenter.selectArgs();
 
-      expect(expected).toEqual(result);
+        expect(expected).toEqual(result);
+      });
     });
-  });
 
-  describe('when given a selected country', () => {
-    it('returns a list of countries, with the given country selected', () => {
-      const presenter = new CountriesSelectPresenter('Morocco');
+    describe('when given a selected country', () => {
+      it('returns a list of countries, with the given country selected', () => {
+        const presenter = new CountriesSelectPresenter('Morocco');
 
-      const expected: SelectItemArgs[] = [
-        {
-          text: '--- Please Select ---',
-          value: '',
-          selected: null,
-        },
-        {
-          text: 'France',
-          value: 'France',
-          selected: false,
-        },
-        {
-          text: 'Belgium',
-          value: 'Belgium',
-          selected: false,
-        },
-        {
-          text: 'Brazil',
-          value: 'Brazil',
-          selected: false,
-        },
-        {
-          text: 'Japan',
-          value: 'Japan',
-          selected: false,
-        },
-        {
-          text: 'Morocco',
-          value: 'Morocco',
-          selected: true,
-        },
-        {
-          text: 'Poland',
-          value: 'Poland',
-          selected: false,
-        },
-        {
-          text: 'Germany',
-          value: 'Germany',
-          selected: false,
-        },
-        {
-          text: 'Italy',
-          value: 'Italy',
-          selected: false,
-        },
-        {
-          text: 'Canada',
-          value: 'Canada',
-          selected: false,
-        },
-      ];
-      const result = presenter.selectArgs();
+        const expected: SelectItemArgs[] = [
+          {
+            text: '--- Please Select ---',
+            value: '',
+            selected: null,
+          },
+          {
+            text: 'France',
+            value: 'France',
+            selected: false,
+          },
+          {
+            text: 'Belgium',
+            value: 'Belgium',
+            selected: false,
+          },
+          {
+            text: 'Brazil',
+            value: 'Brazil',
+            selected: false,
+          },
+          {
+            text: 'Japan',
+            value: 'Japan',
+            selected: false,
+          },
+          {
+            text: 'Morocco',
+            value: 'Morocco',
+            selected: true,
+          },
+          {
+            text: 'Poland',
+            value: 'Poland',
+            selected: false,
+          },
+          {
+            text: 'Germany',
+            value: 'Germany',
+            selected: false,
+          },
+          {
+            text: 'Italy',
+            value: 'Italy',
+            selected: false,
+          },
+          {
+            text: 'Canada',
+            value: 'Canada',
+            selected: false,
+          },
+        ];
+        const result = presenter.selectArgs();
 
-      expect(expected).toEqual(result);
+        expect(expected).toEqual(result);
+      });
     });
   });
 });
