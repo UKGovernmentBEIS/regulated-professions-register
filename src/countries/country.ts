82caa8a5184a1f2dca997d0962b6ab7979483cad
Bryan Gale <bryan.gale@dxw.com>
2022-04-20 15:40:47 +0100

    Add a `Country` class
    This works similarly to our `Nation` class, and is used for getting a list of countries, or finding a single country

---
+++ src/countries/country.ts
@@ -0,0 +1,51 @@
+import { I18nService } from 'nestjs-i18n';
+
+export class Country {
+  private static allCountries: readonly Country[];
+  private static allCountriesByCode: Map<string, Country>;
+
+  readonly name: string;
+  readonly code: string;
+
+  constructor(name: string, code: string) {
+    this.name = name;
+    this.code = code;
+  }
+
+  translatedName(i18nService: I18nService): string {
+    return i18nService.translate<string>(this.name);
+  }
+
+  static all(): readonly Country[] {
+    if (!this.allCountries) {
+      const rawCountries: {
+        name: string;
+        code: string;
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+      }[] = require('../config/countries.json');
+
+      Country.allCountries = rawCountries.map(
+        (rawCountry) => new Country(rawCountry.name, rawCountry.code),
+      );
+    }
+
+    return Country.allCountries;
+  }
+
+  static find(code: string): Country {
+    if (!this.allCountriesByCode) {
+      Country.allCountriesByCode = new Map();
+      this.all().forEach((country) => {
+        this.allCountriesByCode.set(country.code, country);
+      });
+    }
+
+    const selectedCountry = this.allCountriesByCode.get(code);
+
+    if (!selectedCountry) {
+      throw new Error('Could not find requested Country');
+    }
+
+    return selectedCountry;
+  }
+}
