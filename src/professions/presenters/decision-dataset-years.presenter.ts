eb398765517daccb58fa7c18e9eb789078a9c625
Bryan Gale <bryan.gale@dxw.com>
2022-04-28 18:05:24 +0100

    Add `DecisionDatasetYearsPresenter`
    We use this to generate the `SummaryList` of years to display on public profession pages

---
+++ src/professions/presenters/decision-dataset-years.presenter.ts
@@ -0,0 +1,33 @@
+import { I18nService } from 'nestjs-i18n';
+import { SummaryList } from '../../common/interfaces/summary-list';
+import { Profession } from '../profession.entity';
+
+export class DecisionDatasetYearsPresenter {
+  constructor(
+    private readonly years: number[],
+    private readonly profession: Profession,
+    private readonly i18nService: I18nService,
+  ) {}
+
+  summaryList(): SummaryList {
+    const key = {
+      text: this.i18nService.translate<string>(
+        'professions.show.decisions.year',
+      ),
+    };
+
+    return {
+      classes: 'govuk-summary-list--no-border',
+      rows: this.years.map((year) => ({
+        key,
+        value: {
+          html: `<a href="${this.link(year)}" class="govuk-link">${year}</a>`,
+        },
+      })),
+    };
+  }
+
+  link(year: number): string {
+    return `/decisions/${this.profession.slug}/${year}`;
+  }
+}
