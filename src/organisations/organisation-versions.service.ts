7059814e655996661931fba747802f3b5d95c87f
rich <rich.james@dxw.com>
2022-05-18 11:37:44 +0100

    Remove distinctOn clause
    This was causing an incomplete amount of professions to sometimes come back from the query
    
    Now we are filtering for only 'live' professions it should be safe to remove

--- src/organisations/organisation-versions.service.ts
+++ src/organisations/organisation-versions.service.ts
@@ -114,7 +114,6 @@ export class OrganisationVersionsService {
 
   async searchLive(filter: FilterInput): Promise<Organisation[]> {
     const query = this.versionsWithJoins([ProfessionVersionStatus.Live])
-      .distinctOn(['organisation.name', 'organisation'])
       .orderBy('organisation.name, organisation')
       .where('organisationVersion.status = :status', {
         status: OrganisationVersionStatus.Live,
