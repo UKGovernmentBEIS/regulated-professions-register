157e0d5dccb232641fdb1a70fd7597da8a226af2
Bryan Gale <bryan.gale@dxw.com>
2022-05-30 15:14:31 +0100

    Rearrange `DecisionDataValidator` tests ahead of adding further functionality
    We combine redundant "no errors" tests into a single test

--- src/helpers/decision-data-validator.spec.ts
+++ src/helpers/decision-data-validator.spec.ts
@@ -1,103 +1,81 @@
+import { EditDto } from '../decisions/admin/dto/edit.dto';
 import { DecisionDataValidator } from './decision-data-validator';
 
 describe('DecisionDataValidator', () => {
-  describe('validating no empty routes', () => {
-    describe('when there are no empty routes submitted', () => {
-      it('returns an empty array of errors', () => {
-        const editDto = {
-          routes: ['Route 1', 'Route 2'],
-          countries: [['CA'], ['AR']],
-          yeses: [['1'], ['1']],
-          yesAfterComps: [['1'], ['1']],
-          noes: [['1'], ['1']],
-          noAfterComps: [['1'], ['1']],
-          action: 'save',
-        };
+  describe('valid data is submitted', () => {
+    it('returns an empty array of errors', () => {
+      const editDto: EditDto = {
+        routes: ['Route 1', 'Route 2'],
+        countries: [['CA'], ['AR']],
+        yeses: [['1'], ['1']],
+        yesAfterComps: [['1'], ['1']],
+        noes: [['1'], ['1']],
+        noAfterComps: [['1'], ['1']],
+        action: 'save',
+      };
 
-        const validated = DecisionDataValidator.validate(editDto);
+      const validated = DecisionDataValidator.validate(editDto);
 
-        expect(validated.valid()).toEqual(true);
-        expect(validated.errors).toEqual([]);
-      });
-    });
-
-    describe('when empty routes are submitted', () => {
-      it('returns an array of errors', () => {
-        const editDto = {
-          routes: ['', 'Route 2', ' '],
-          countries: [['CA'], ['AR'], ['JP']],
-          yeses: [['1'], ['1'], ['1']],
-          yesAfterComps: [['1'], ['1'], ['1']],
-          noes: [['1'], ['1'], ['1']],
-          noAfterComps: [['1'], ['1'], ['1']],
-          action: 'save',
-        };
-
-        const validated = DecisionDataValidator.validate(editDto);
-
-        expect(validated.valid()).toEqual(false);
-        expect(validated.errors).toEqual([
-          {
-            constraints: {
-              message: 'decisions.admin.edit.errors.routes.empty',
-            },
-            property: 'routes[1]',
-          },
-          {
-            constraints: {
-              message: 'decisions.admin.edit.errors.routes.empty',
-            },
-            property: 'routes[3]',
-          },
-        ]);
-      });
+      expect(validated.valid()).toEqual(true);
+      expect(validated.errors).toEqual([]);
     });
   });
 
-  describe('validating no duplicate routes', () => {
-    describe('when there are no duplicate routes submitted', () => {
-      it('returns an empty array of errors', () => {
-        const editDto = {
-          routes: ['Route 1', 'Route 2'],
-          countries: [['CA'], ['AR']],
-          yeses: [['1'], ['1']],
-          yesAfterComps: [['1'], ['1']],
-          noes: [['1'], ['1']],
-          noAfterComps: [['1'], ['1']],
-          action: 'save',
-        };
+  describe('when empty routes are submitted', () => {
+    it('returns an array of errors, with the empty routes highlighted', () => {
+      const editDto: EditDto = {
+        routes: ['', 'Route 2', ' '],
+        countries: [['CA'], ['AR'], ['JP']],
+        yeses: [['1'], ['1'], ['1']],
+        yesAfterComps: [['1'], ['1'], ['1']],
+        noes: [['1'], ['1'], ['1']],
+        noAfterComps: [['1'], ['1'], ['1']],
+        action: 'save',
+      };
 
-        const validated = DecisionDataValidator.validate(editDto);
+      const validated = DecisionDataValidator.validate(editDto);
 
-        expect(validated.valid()).toEqual(true);
-        expect(validated.errors).toEqual([]);
-      });
-    });
-
-    describe('when duplicate routes are submitted', () => {
-      it('returns an array of errors, with the duplicate route highlighted', () => {
-        const editDto = {
-          routes: ['Route 1', 'Route 2', 'Route 1'],
-          countries: [['CA'], ['AR'], ['JP']],
-          yeses: [['1'], ['1'], ['1']],
-          yesAfterComps: [['1'], ['1'], ['1']],
-          noes: [['1'], ['1'], ['1']],
-          noAfterComps: [['1'], ['1'], ['1']],
-          action: 'save',
-        };
-
-        const validated = DecisionDataValidator.validate(editDto);
-
-        expect(validated.valid()).toEqual(false);
-        expect(validated.errors).toEqual([
-          {
-            constraints: {
-              message: 'decisions.admin.edit.errors.routes.duplicate',
-            },
-            property: 'routes[3]',
+      expect(validated.valid()).toEqual(false);
+      expect(validated.errors).toEqual([
+        {
+          constraints: {
+            message: 'decisions.admin.edit.errors.routes.empty',
           },
-        ]);
-      });
+          property: 'routes[1]',
+        },
+        {
+          constraints: {
+            message: 'decisions.admin.edit.errors.routes.empty',
+          },
+          property: 'routes[3]',
+        },
+      ]);
+    });
+  });
+
+  describe('when duplicate routes are submitted', () => {
+    it('returns an array of errors, with the duplicate route highlighted', () => {
+      const editDto: EditDto = {
+        routes: ['Route 1', 'Route 2', 'Route 1'],
+        countries: [['CA'], ['AR'], ['JP']],
+        yeses: [['1'], ['1'], ['1']],
+        yesAfterComps: [['1'], ['1'], ['1']],
+        noes: [['1'], ['1'], ['1']],
+        noAfterComps: [['1'], ['1'], ['1']],
+        action: 'save',
+      };
+
+      const validated = DecisionDataValidator.validate(editDto);
+
+      expect(validated.valid()).toEqual(false);
+      expect(validated.errors).toEqual([
+        {
+          constraints: {
+            message: 'decisions.admin.edit.errors.routes.duplicate',
+          },
+          property: 'routes[3]',
+        },
+      ]);
     });
   });
 });
