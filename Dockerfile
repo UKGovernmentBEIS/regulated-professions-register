64b55b4679a8631650c41531efb4368ab310dda4
James Holder <james.holder@cgi.com>
2023-03-08 10:47:46 +0000

    update terraform scripts

--- Dockerfile
+++ Dockerfile
@@ -96,3 +96,17 @@ COPY --from=dependencies ${DEPS_HOME}/node_modules ${TEST_HOME}/node_modules
 COPY --from=dependencies /root/.cache/Cypress /root/.cache/Cypress
 
 COPY --from=web ${APP_HOME} ${TEST_HOME}
+
+# ------------------------------------------------------------------------------
+# nginx Redirect
+# ------------------------------------------------------------------------------
+
+FROM nginx:1.23.3-alpine-slim AS redirect
+
+COPY script/redirect /usr/local/bin/
+
+RUN chmod +x /usr/local/bin/redirect
+
+EXPOSE 80
+
+CMD ["redirect"]
---
+++ terraform/redirect.tf
@@ -0,0 +1,24 @@
+# Create the web app.
+
+resource "cloudfoundry_app" "beis-rpr-redirect" {
+  name         = "beis-rpr-redirect-${var.environment}"
+  space        = cloudfoundry_space.space.id
+  instances    = 2
+  disk_quota   = 1024
+  memory       = 256
+  timeout      = 300
+  docker_image = "ghcr.io/ukgovernmentbeis/regulated-professions-register-redirect:${var.docker_tag}"
+  strategy                   = "blue-green-v2"
+  health_check_http_endpoint = "/"
+  environment = {
+    "REDIRECT_TARGET"            = "https://${var.custom_hostname}.${var.custom_domain}/"
+  }
+  # routes need to be declared with the app for blue green deployments to work
+  routes {
+    route = cloudfoundry_route.beis-rpr-route.id
+  }
+  routes {
+    route = cloudfoundry_route.beis-rpr-redirect-route.id
+  }
+
+}
