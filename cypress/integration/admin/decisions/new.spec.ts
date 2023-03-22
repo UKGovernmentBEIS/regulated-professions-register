126f76f384507cd971b3636746ca22bd487e42b6
James Holder <james.holder@cgi.com>
2023-03-22 14:30:29 +0000

    temporarily remove decision tests for org users

--- cypress/integration/admin/decisions/new.spec.ts
+++ cypress/integration/admin/decisions/new.spec.ts
@@ -245,99 +245,99 @@ describe('Creating a decision dataset', () => {
     });
   });
 
-  context('When I am logged in as an organisation editor', () => {
-    beforeEach(() => {
-      cy.loginAuth0('orgeditor');
-      cy.visitInternalDashboard();
-      cy.translate('app.pages.admin.dashboard.editDecisionDataRegulators').then(
-        (link) => {
-          cy.get('a').contains(link).click();
-          cy.checkAccessibility();
-        },
-      );
-    });
+  // context('When I am logged in as an organisation editor', () => {
+  //   beforeEach(() => {
+  //     cy.loginAuth0('orgeditor');
+  //     cy.visitInternalDashboard();
+  //     cy.translate('app.pages.admin.dashboard.editDecisionDataRegulators').then(
+  //       (link) => {
+  //         cy.get('a').contains(link).click();
+  //         cy.checkAccessibility();
+  //       },
+  //     );
+  //   });
 
-    it('I can create a decision dataset for my organisation', () => {
-      cy.get('tbody').should('not.contain', '2020');
+  //   it('I can create a decision dataset for my organisation', () => {
+  //     cy.get('tbody').should('not.contain', '2020');
 
-      cy.translate('decisions.admin.dashboard.addButtonLabel').then((add) => {
-        cy.get('a').contains(add).click();
-      });
+  //     cy.translate('decisions.admin.dashboard.addButtonLabel').then((add) => {
+  //       cy.get('a').contains(add).click();
+  //     });
 
-      cy.checkAccessibility();
+  //     cy.checkAccessibility();
 
-      cy.translate('decisions.admin.new.labels.profession').then(
-        (profession) => {
-          cy.get('label')
-            .contains(profession)
-            .parent()
-            .within(() => {
-              cy.get('select').select(
-                'Secondary School Teacher in State maintained schools (England)',
-              );
-            });
-        },
-      );
+  //     cy.translate('decisions.admin.new.labels.profession').then(
+  //       (profession) => {
+  //         cy.get('label')
+  //           .contains(profession)
+  //           .parent()
+  //           .within(() => {
+  //             cy.get('select').select(
+  //               'Secondary School Teacher in State maintained schools (England)',
+  //             );
+  //           });
+  //       },
+  //     );
 
-      cy.translate('decisions.admin.new.labels.year').then((profession) => {
-        cy.get('label')
-          .contains(profession)
-          .parent()
-          .within(() => {
-            cy.get('select').select('2020');
-          });
-      });
+  //     cy.translate('decisions.admin.new.labels.year').then((profession) => {
+  //       cy.get('label')
+  //         .contains(profession)
+  //         .parent()
+  //         .within(() => {
+  //           cy.get('select').select('2020');
+  //         });
+  //     });
 
-      cy.translate('app.continue').then((continueLabel) => {
-        cy.get('button').contains(continueLabel).click();
-      });
+  //     cy.translate('app.continue').then((continueLabel) => {
+  //       cy.get('button').contains(continueLabel).click();
+  //     });
 
-      cy.get('h1').should(
-        'contain',
-        'Secondary School Teacher in State maintained schools (England)',
-      );
+  //     cy.get('h1').should(
+  //       'contain',
+  //       'Secondary School Teacher in State maintained schools (England)',
+  //     );
 
-      cy.checkSummaryListRowValue(
-        'decisions.admin.edit.regulator',
-        'Department for Education',
-      );
-      cy.checkSummaryListRowValue('decisions.admin.edit.year', '2020');
+  //     cy.checkSummaryListRowValue(
+  //       'decisions.admin.edit.regulator',
+  //       'Department for Education',
+  //     );
+  //     cy.checkSummaryListRowValue('decisions.admin.edit.year', '2020');
 
-      cy.get('input[name="routes[1]"]').type('New route');
-      cy.get('select[name="countries[1][1]"]').select('Brazil');
-      cy.get('input[name="yeses[1][1]"]').type('8');
-      cy.get('input[name="noes[1][1]"]').type('1');
-      cy.get('input[name="yesAfterComps[1][1]"]').type('2');
-      cy.get('input[name="noAfterComps[1][1]"]').type('3');
+  //     cy.get('input[name="routes[1]"]').type('New route');
+  //     cy.get('select[name="countries[1][1]"]').select('Brazil');
+  //     cy.get('input[name="yeses[1][1]"]').type('8');
+  //     cy.get('input[name="noes[1][1]"]').type('1');
+  //     cy.get('input[name="yesAfterComps[1][1]"]').type('2');
+  //     cy.get('input[name="noAfterComps[1][1]"]').type('3');
 
-      cy.translate('decisions.admin.buttons.saveAsDraft').then((save) => {
-        cy.get('button').contains(save).click();
-      });
+  //     cy.translate('decisions.admin.buttons.saveAsDraft').then((save) => {
+  //       cy.get('button').contains(save).click();
+  //     });
 
-      cy.get('caption').should('contain', 'New route');
+  //     cy.get('caption').should('contain', 'New route');
 
-      cy.checkVerticalTable(
-        [
-          'decisions.show.tableHeading.country',
-          'decisions.show.tableHeading.yes',
-          'decisions.show.tableHeading.yesAfterComp',
-          'decisions.show.tableHeading.no',
-          'decisions.show.tableHeading.noAfterComp',
-        ],
-        [['Brazil', '8', '2', '1', '3']],
-      );
+  //     cy.checkVerticalTable(
+  //       [
+  //         'decisions.show.tableHeading.country',
+  //         'decisions.show.tableHeading.yes',
+  //         'decisions.show.tableHeading.yesAfterComp',
+  //         'decisions.show.tableHeading.no',
+  //         'decisions.show.tableHeading.noAfterComp',
+  //       ],
+  //       [['Brazil', '8', '2', '1', '3']],
+  //     );
 
-      cy.visitAndCheckAccessibility('/admin/decisions');
+  //     cy.visitAndCheckAccessibility('/admin/decisions');
 
-      cy.get('td')
-        .contains('2020')
-        .parent()
-        .parent()
-        .within(($row) => {
-          cy.translate('app.status.draft').then((draft) => {
-            cy.wrap($row).contains(draft);
-          });
-        });
-    });
-  });
+  //     cy.get('td')
+  //       .contains('2020')
+  //       .parent()
+  //       .parent()
+  //       .within(($row) => {
+  //         cy.translate('app.status.draft').then((draft) => {
+  //           cy.wrap($row).contains(draft);
+  //         });
+  //       });
+  //   });
+  // });
 });
--- cypress/integration/admin/decisions/submit.spec.ts
+++ cypress/integration/admin/decisions/submit.spec.ts
@@ -1,87 +1,87 @@
 describe('Submiting a decision dataset', () => {
-  context('When I am logged in as an organisation-level user', () => {
-    beforeEach(() => {
-      cy.loginAuth0('orgeditor');
-      cy.visitInternalDashboard();
-      cy.translate('app.pages.admin.dashboard.editDecisionDataRegulators').then(
-        (link) => {
-          cy.get('a').contains(link).click();
-          cy.checkAccessibility();
-        },
-      );
-    });
+  // context('When I am logged in as an organisation-level user', () => {
+  //   beforeEach(() => {
+  //     cy.loginAuth0('orgeditor');
+  //     cy.visitInternalDashboard();
+  //     cy.translate('app.pages.admin.dashboard.editDecisionDataRegulators').then(
+  //       (link) => {
+  //         cy.get('a').contains(link).click();
+  //         cy.checkAccessibility();
+  //       },
+  //     );
+  //   });
 
-    it('I can submit a draft decision dataset', () => {
-      cy.get('tr')
-        .contains(
-          'Secondary School Teacher in State maintained schools (England)',
-        )
-        .parent()
-        .within(() => {
-          cy.translate('app.status.draft').then((draft) => {
-            cy.contains(draft);
-          });
-          cy.get('a').contains('View details').click();
-        });
+  //   it('I can submit a draft decision dataset', () => {
+  //     cy.get('tr')
+  //       .contains(
+  //         'Secondary School Teacher in State maintained schools (England)',
+  //       )
+  //       .parent()
+  //       .within(() => {
+  //         cy.translate('app.status.draft').then((draft) => {
+  //           cy.contains(draft);
+  //         });
+  //         cy.get('a').contains('View details').click();
+  //       });
 
-      cy.translate('decisions.admin.buttons.submit').then((submitButton) => {
-        cy.get('a').contains(submitButton).click();
-      });
+  //     cy.translate('decisions.admin.buttons.submit').then((submitButton) => {
+  //       cy.get('a').contains(submitButton).click();
+  //     });
 
-      cy.checkAccessibility();
+  //     cy.checkAccessibility();
 
-      cy.translate('decisions.admin.submission.caption').then(
-        (submitCaption) => {
-          cy.get('body').contains(submitCaption);
-        },
-      );
+  //     cy.translate('decisions.admin.submission.caption').then(
+  //       (submitCaption) => {
+  //         cy.get('body').contains(submitCaption);
+  //       },
+  //     );
 
-      cy.translate('decisions.admin.submission.heading').then((heading) => {
-        cy.contains(heading);
-      });
+  //     cy.translate('decisions.admin.submission.heading').then((heading) => {
+  //       cy.contains(heading);
+  //     });
 
-      cy.contains(
-        'Secondary School Teacher in State maintained schools (England)',
-      );
-      cy.contains('2019');
+  //     cy.contains(
+  //       'Secondary School Teacher in State maintained schools (England)',
+  //     );
+  //     cy.contains('2019');
 
-      cy.translate('decisions.admin.buttons.submit').then((submitButton) => {
-        cy.get('button').contains(submitButton).click();
-      });
+  //     cy.translate('decisions.admin.buttons.submit').then((submitButton) => {
+  //       cy.get('button').contains(submitButton).click();
+  //     });
 
-      cy.checkAccessibility();
+  //     cy.checkAccessibility();
 
-      cy.translate('decisions.show.heading').then((heading) => {
-        cy.get('body').should('contain', heading);
-      });
+  //     cy.translate('decisions.show.heading').then((heading) => {
+  //       cy.get('body').should('contain', heading);
+  //     });
 
-      cy.translate('decisions.admin.submission.confirmation.heading').then(
-        (confirmationHeading) => {
-          cy.get('body').should('contain', confirmationHeading);
-        },
-      );
+  //     cy.translate('decisions.admin.submission.confirmation.heading').then(
+  //       (confirmationHeading) => {
+  //         cy.get('body').should('contain', confirmationHeading);
+  //       },
+  //     );
 
-      // Temporary check until we display the status on the "show" page
-      cy.visitAndCheckAccessibility('/admin/decisions');
+  //     // Temporary check until we display the status on the "show" page
+  //     cy.visitAndCheckAccessibility('/admin/decisions');
 
-      cy.get('tr')
-        .contains(
-          'Secondary School Teacher in State maintained schools (England)',
-        )
-        .parent()
-        .within(() => {
-          cy.translate('app.status.submitted').then((submitted) => {
-            cy.contains(submitted);
-            cy.get('a').contains('View details').click();
-          });
-        });
+  //     cy.get('tr')
+  //       .contains(
+  //         'Secondary School Teacher in State maintained schools (England)',
+  //       )
+  //       .parent()
+  //       .within(() => {
+  //         cy.translate('app.status.submitted').then((submitted) => {
+  //           cy.contains(submitted);
+  //           cy.get('a').contains('View details').click();
+  //         });
+  //       });
 
-      cy.checkAccessibility();
-      cy.translate('decisions.admin.buttons.submit').then((submit) => {
-        cy.get('a').should('not.contain', submit);
-      });
-    });
-  });
+  //     cy.checkAccessibility();
+  //     cy.translate('decisions.admin.buttons.submit').then((submit) => {
+  //       cy.get('a').should('not.contain', submit);
+  //     });
+  //   });
+  // });
 
   context('When I am logged in as a central admin user', () => {
     beforeEach(() => {
