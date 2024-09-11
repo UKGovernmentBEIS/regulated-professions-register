a072a3d4c0f63171fce7319783bf9a41bb9b1eee
James Holder <james.holder@cgi.com>
2024-09-11 11:57:12 +0100

    Fix get-tests-for-matrix

--- util/get-tests-for-matrix.ts
+++ util/get-tests-for-matrix.ts
@@ -3,18 +3,19 @@ import { hideBin } from 'yargs/helpers';
 
 import yargs from 'yargs';
 
-const argv = yargs(hideBin(process.argv)).argv;
-const numberOfChunks = argv.chunks as number;
-const matrix = argv.matrix as number;
+const argv = yargs(hideBin(process.argv)).argv as { chunks?: number, matrix?: number };
+
+if (typeof argv.chunks !== 'number' || typeof argv.matrix !== 'number') {
+  throw new Error('Both chunks and matrix arguments must be provided and must be numbers.');
+}
+
+const numberOfChunks = argv.chunks;
+const matrix = argv.matrix;
 
 glob('./cypress/integration/**/*.spec.ts', {}, (_er, files) => {
-  const res = [];
+  const res: string[][] = [];
 
-  for (
-    let remainingChunks = numberOfChunks;
-    remainingChunks > 0;
-    remainingChunks--
-  ) {
+  for (let remainingChunks = numberOfChunks; remainingChunks > 0; remainingChunks--) {
     const chunkLength = Math.round(files.length / remainingChunks);
     const chunk = files.splice(0, chunkLength);
     res.push(chunk);
