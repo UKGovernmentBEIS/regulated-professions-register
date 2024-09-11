15de5b8ab2a55aa1ad80303f813495307a8b3c9b
James Holder <james.holder@cgi.com>
2024-09-11 11:57:33 +0100

    fix formatting

--- util/get-tests-for-matrix.ts
+++ util/get-tests-for-matrix.ts
@@ -3,10 +3,15 @@ import { hideBin } from 'yargs/helpers';
 
 import yargs from 'yargs';
 
-const argv = yargs(hideBin(process.argv)).argv as { chunks?: number, matrix?: number };
+const argv = yargs(hideBin(process.argv)).argv as {
+  chunks?: number;
+  matrix?: number;
+};
 
 if (typeof argv.chunks !== 'number' || typeof argv.matrix !== 'number') {
-  throw new Error('Both chunks and matrix arguments must be provided and must be numbers.');
+  throw new Error(
+    'Both chunks and matrix arguments must be provided and must be numbers.',
+  );
 }
 
 const numberOfChunks = argv.chunks;
@@ -15,7 +20,11 @@ const matrix = argv.matrix;
 glob('./cypress/integration/**/*.spec.ts', {}, (_er, files) => {
   const res: string[][] = [];
 
-  for (let remainingChunks = numberOfChunks; remainingChunks > 0; remainingChunks--) {
+  for (
+    let remainingChunks = numberOfChunks;
+    remainingChunks > 0;
+    remainingChunks--
+  ) {
     const chunkLength = Math.round(files.length / remainingChunks);
     const chunk = files.splice(0, chunkLength);
     res.push(chunk);
